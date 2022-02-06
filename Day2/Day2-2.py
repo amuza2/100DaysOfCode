ea17b1dbd4c1c5fef81a1d0c9ed90566fb9e246e
Mohammed Chami <mohamuza@gmail.com>
2022-02-06 23:54:12 +0100

    added day2

---
+++ Day2/Day2-2.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import pandas as pd
+
+website = "https://www.audible.com/search"
+path = r"C:/SeleniumDrivers/chromedriver.exe"
+driver = webdriver.Chrome(path)
+driver.get(website)
+#driver.maximize_window()
+
+container = driver.find_element_by_class_name("adbl-impression-container ")
+products = driver.find_elements_by_css_selector(".productListItem")
+
+book_title, book_author, book_length = [], [], []
+
+title = "h3.bc-heading.bc-color-link.bc-pub-break-word.bc-size-medium"
+author = "li.bc-list-item.authorLabel a"
+length = "li.bc-list-item.narratorLabel a"
+str_author = " "
+for product in products:
+	book_title.append(product.find_element_by_css_selector(title).text)
+
+	book_author.append(" ".join([f"{i.text}" for i in product.find_elements_by_css_selector(author)]))
+	
+	book_length.append(" ".join([f"{j.text}" for j in product.find_elements_by_css_selector(length)]))
+
+driver.quit()
+
+my_audio = {"title":book_title, "author":book_author, "length":book_length}
+df = pd.DataFrame(my_audio)
+df.to_csv("audible.csv", index=False)
+print(df.head())
---
+++ Day2/Day2.md
@@ -0,0 +1,9 @@
+Day 1:
+
+Started learning Selenium
+
+I learned:
+
+1- how to select from drop down menu;
+
+2- how to scrape title,author and length of the audiobook from audible website.
