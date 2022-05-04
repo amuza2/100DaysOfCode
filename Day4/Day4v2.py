6db64640e1c58ddffee29676be789ead60336235
Mohammed Chami <mohamuza@gmail.com>
2022-05-04 21:04:52 +0100

    modified day4 v2

--- Day4/Day4v2.py
+++ Day4/Day4v2.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
+
+option = Options()
+option.headless = True
+option.add_argument("window-size=1600x900")
 
 url = "https://www.audible.com/adblbestsellers"
 path = "C:/SeleniumDrivers/chromedriver.exe"
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome(path,options=option)
 driver.get(url)
-driver.maximize_window()
+# driver.maximize_window()
 
 max_page = int(driver.find_elements_by_css_selector(".linkListWrapper .bc-link.refinementFormLink.pageNumberElement")[1].text)
 l_name, l_author = [],[]
@@ -16,8 +21,11 @@ l_name, l_author = [],[]
 for j in range(max_page):
 	audio_books = driver.find_elements_by_css_selector(".bc-col-responsive.bc-col-6")
 	for item in audio_books:	
-		l_name.append(item.find_element_by_css_selector("h3 a").text)
-		l_author.append(' '.join([i.text for i in item.find_elements_by_css_selector(".bc-list-item.authorLabel a")]))
+		name = item.find_element_by_css_selector("h3 a")
+		author = item.find_elements_by_css_selector(".bc-list-item.authorLabel a")
+
+		l_name.append(name.text)
+		l_author.append(' '.join([i.text for i in author]))
 
 	next_page = driver.find_element_by_css_selector(".bc-button.bc-button-secondary.nextButton")
 	next_page.click()
