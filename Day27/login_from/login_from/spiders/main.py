c065d0c27bf8785cd0b92e0d71d993e862e2fefe
Mohammed Chami <mohamuza@gmail.com>
2022-05-18 16:12:42 +0100

    added day27

---
+++ Day27/login_from/login_from/spiders/main.py
@@ -0,0 +1,22 @@
+import scrapy
+from scrapy import FormRequest
+
+
+class MainSpider(scrapy.Spider):
+    name = 'main'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['https://quotes.toscrape.com/login']
+
+    def authentication_failed(self, response):
+        if response.css(".row.header-box p a::text").get() == "Logout":
+            return True
+
+    def parse(self, response):
+        yield FormRequest.from_response(response,
+            formdata={"username":"admin", "password":"admin"},
+            callback=self.after_login,
+            )
+
+    def after_login(self, response):
+        if self.authentication_failed(response):
+            print("==> Loged in")
