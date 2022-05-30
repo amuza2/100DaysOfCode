1d35eb2a5d53de613488d35aac621c1544b478bf
Mohammed Chami <mohamuza@gmail.com>
2022-05-30 17:44:28 +0100

    added day29

---
+++ Day29/bill_app.py
@@ -0,0 +1,47 @@
+"""
+create an app that calculate the amount of any person
+stayes in a house accourding on the days he/she stayed.
+
+names: amount, person, house, days
+30 days ==> 30 dollars
+15 days ==> x
+x = (15 * 30)/ 30
+"""
+
+
+class Bill:
+    monthly_fee = 120
+    people = []
+    after_calc = []
+
+
+    def __init__(self,name,days):
+        self.people.append([name,days])
+
+
+    def pays(self):
+        split = self.monthly_fee / len(self.people) 
+        for person in self.people:
+            payment = (person[1] * split)/30
+            self.after_calc.append([person[0], payment])
+
+
+    def add_person(self, name, days):
+        self.people.append([name,days])
+
+
+    def show_result(self):
+        self.pays()
+        for i in self.after_calc:
+            print(f"{i[0]} pays {i[1]}")
+
+    def to_pdf(self,filename="Billfile"):
+        with open("{filename}", 'w'):
+
+med = Bill("med", 25)
+med.add_person("amine", 20)
+
+med.show_result()
+
+
+
