728d7100614bdd67aec4146fdfcdea909ced24bf
Mohammed Chami <mohamuza@gmail.com>
2022-06-04 15:37:11 +0100

    updated bill app

--- Day29/bill_app.py
+++ Day29/bill_app.py
@@ -9,38 +9,42 @@ x = (15 * 30)/ 30
 """
 
 import fpdf
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 class Client:
-    def __init__(self, name):
+    def __init__(self, name, phone):
         self.name = name
-        self.day = datetime.
+        self.phone = phone
+        self.date = datetime.strptime(datetime.now().strftime("%Y %m %d"),"%Y %m %d")
 
+    def update_info(self, name, phone, date):
+        self.name = name
+        self.phone = phone
+        date = date.split()
+        self.date = datetime(date[0], date[1], date[2])
 
 class Flat:
-    monthly_fee = 120
-    people = []
-    after_calc = []
+
+    def __init__(self):
+        self.fees = 20000
+        self.people_in_flat = {}
+        self.days_in_month = 30
 
 
-    def __init__(self,client,days):
-        self.
-
-    def pays(self):
-        split = self.monthly_fee / len(self.people) 
-        for person in self.people:
-            payment = (person[1] * split)/30
-            self.after_calc.append([person[0], payment])
+    def add_person(self, client):
+        self.people_in_flat[client.name] = client
+        print(f"Client {client.name} has been added")
 
 
-    def add_person(self, name, days):
-        self.people.append([name,days])
+    def bill(self):
+        part = self.fees / len(self.people_in_flat.keys())
 
+        for i in self.people_in_flat.values():
+            days_in_flat = datetime(i.date.year, i.date.month+1, 1) - i.date
+            c = (part * days_in_flat.days) / self.days_in_month
+            print(f"{i.name} spent {days_in_flat.days} days, he has to pay: {c}")
 
-    def show_result(self):
-        self.pays()
-        for i in self.after_calc:
-            print(f"{i[0]} pays {i[1]}")
 
     def to_pdf(self,filename="Billfile"):
         pass
@@ -50,5 +54,15 @@ class CreatePDFReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def to_pdf(self, flatmate)
+    def to_pdf(self, flatmate):
+        pass
 
+flat = Flat()
+
+med = Client("med", 1234)
+zim = Client("zim", 5678)
+zim.update_info(date="2022 06 15")
+
+# flat.add_person(med)
+# flat.add_person(zim)
+# flat.bill()
