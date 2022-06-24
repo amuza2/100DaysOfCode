c04f531eb40c296daea30a105c7a04f5e8df53dc
Mohammed Chami <mohamuza@gmail.com>
2022-06-24 19:07:16 +0100

    updated day32

--- Day32/main.py
+++ Day32/main.py
@@ -1,4 +1,23 @@
 """
-create a gui app that showes images
+create a gui app that showes image when you write a name
 
 """
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file("frontend.kv")
+
+class FirstScreen(Screen):
+	def search_image(self):
+		pass
+
+class RootWidget(ScreenManager):
+	pass
+
+class MainApp(App):
+	
+	def build(self):
+		return RootWidget()
+
+MainApp().run()
