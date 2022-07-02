cfa092a74692aeeb3faf935ae050ba938fd2ca78
Mohammed Chami <mohamuza@gmail.com>
2022-07-02 21:00:36 +0100

    updated day32

--- Day32/main.py
+++ Day32/main.py
@@ -2,6 +2,7 @@
 create a gui app that showes image when you write a name
 
 """
+import wikipedia
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -10,7 +11,15 @@ Builder.load_file("frontend.kv")
 
 class FirstScreen(Screen):
 	def search_image(self):
-		self.manager.current_screen.ids.img.source = "analytics.png"
+		query = self.manager.current_screen.ids.user_query.text
+		page = wikipedia.page(query)
+		image_link = page.images[0]
+		req = request.get(image_link)
+		with open("file/image.jpeg", "wb") as file:
+			file.write(req.content)
+
+
+		# self.manager.current_screen.ids.img.source = "analytics.png"
 
 class RootWidget(ScreenManager):
 	pass
