642a75707933f162c45ac797bc61efe86c0d7c72
Mohammed Chami <mohamuza@gmail.com>
2022-07-07 19:48:06 +0100

    updated day 33

--- Day33/main.py
+++ Day33/main.py
@@ -1,6 +1,8 @@
 import time
 import random
 import ipdb
+import os
+import json
 
 class Sprite():
 	def __init__(self, life, defence, damage):
@@ -12,22 +14,23 @@ class Sprite():
 		self.damage = damage
 		self.critical = 50
 		self.penetration = 60
+		self.boss_level = 1
 
 	def attack(self, user):
 		if random.randint(1, 100) <= self.critical:
 			if random.randint(1, 100) <= self.penetration:
 				user.life -= abs(self.damage*2)
-				print("critical and penetration used")
+				# print("critical and penetration used")
 			else:	
 				user.life -= abs((self.damage - user.defence)*2)
-				print("critical used")
+				# print("critical used")
 		else:
 			if random.randint(1, 100) <= self.penetration:
 				user.life -= self.damage
-				print("penetration used")
+				# print("penetration used")
 			else:
 				user.life -= abs(self.damage - user.defence)
-				print("no skill used")
+				# print("no skill used")
 
 class Hero(Sprite):
 	pass
@@ -42,6 +45,28 @@ class Boss(Sprite):
 
 hero = Hero(150, 30, 40)
 crops = Creature(50, 5, 20)
+
+def save_game():
+	data = {"level":hero.level,"max_life":hero.max_life,"exp":hero.exp,"life":hero.life,"defence":hero.defence,"damage":hero.damage,"critical":hero.critical,"penetration":hero.penetration,"boss_level":hero.boss_level}
+	with open("game_saved.json", 'w') as f:
+		json.dump(data, f)
+		
+def load_game():
+	with open("game_saved.json") as f:
+		data = json.load(f)
+	
+	hero.level = data["level"]
+	hero.life = data["life"]
+	hero.max_life = data["max_life"]
+	hero.exp = data["exp"]
+	hero.defence = data["defence"]
+	hero.damage = data["damage"]
+	hero.critical = data["critical"]
+	hero.penetration = data["penetration"]
+	hero.boss_level = data["boss_level"]
+
+def display_game_info(user):
+	print(f"\tlife: {user.life}\n\tdamage: {user.damage}\n\tdefence: {user.defence}\n\tLevel: {user.level}")
 
 def level_up():
 	hero.life = hero.max_life
@@ -94,6 +119,11 @@ def attack_creature():
 	if situation == 2:
 		while True:
 			print("*---------------------------*")
+			print("HERO:")
+			display_game_info(hero)
+			print("Crops:")
+			display_game_info(crops)
+			print()
 			print("you are attacking creature...")
 			# time.sleep(2)
 			print("crop life is", crops.life)
@@ -159,24 +189,80 @@ def attack_creature():
 				hero.life = hero.max_life
 				break
 
+def boss_attack():
+	boss = Sprite(100+10*hero.boss_level, 20+2*hero.boss_level, 35+3*hero.boss_level)
+	print("HERO:")
+	display_game_info(hero)
+	print("BOSS:")
+	display_game_info(boss)
+	print("""
+		Do you want 
+		  1- fast fight?
+		  2- detailed fight?
+		""")
+	situation = int(input())
+	if situation == 2:
+		while True:
+			print("*---------------------------*")
+			print("*                           *")
+			print("you are attacking Boss...")
+			print("Boss life is", boss.life)
+			hero.attack(boss)
+			print("Boss life is", boss.life)
+			if boss.life<=0:
+				print("Boss dead you won!")
+				if hero.boss_level<=100:
+					hero.boss_level += 1
+				else:
+					print("You Finished the game!")
+				break
+
+			print("Boss is attacking you...")
+			print("hero life is", hero.life)
+			boss.attack(hero)
+			print("hero life is", hero.life)
+			
+			if hero.life<=0:
+				print("you are dead, Boss killed you!")
+				hero.life = hero.max_life
+				break
+			print("*                           *")
+			print("*---------------------------*")
+	else:
+		while True:
+			hero.attack(boss)
+			if boss.life<=0:
+				print("Boss dead you won!")
+				hero.boss_level += 1
+				break
+			boss.attack(hero)			
+			if hero.life<=0:
+				print("you are dead, Boss killed you!")
+				hero.life = hero.max_life
+				break
+
 def main():
 	while True:
 		print("""
 			1- Attack Creature
 			2- Attack Boss
-			3- Exit
+			3- Save game
+			4- load game
+			5- Exit
 
 			what do you want to attack?
 			""")
 		choice = int(input())
+		os.system("cls")
 		if choice == 1:
 			attack_creature()
 		elif choice == 2:
-			print("not yet")
+			boss_attack()
+		elif choice == 3:
+			save_game()
+		elif choice == 4:
+			load_game()
 		else:
 			break
-
-	
-	
 
 main()
