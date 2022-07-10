8112bea6bd4bc2889aa5ab5a587c7e9a7a5001b1
Mohammed Chami <mohamuza@gmail.com>
2022-07-10 22:04:20 +0100

    added day 34

---
+++ Day34/main.py
@@ -0,0 +1,26 @@
+
+
+class Webcam:
+	def __init__(self):
+		pass
+
+	def draw():
+		pass
+
+	def start_recording(self):
+		pass
+
+	def stop(self):
+		pass
+
+	def capture(self):
+		pass
+
+	def generate_link(self):
+		pass
+
+	def copy_link(self):
+		pass
+
+	def open_in_browser(self):
+		pass
