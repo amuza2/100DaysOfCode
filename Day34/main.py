70cc57bd8bd553717732593ba3b89f7ba3ed1631
Mohammed Chami <mohamuza@gmail.com>
2022-07-15 11:55:32 +0100

    updated day34

--- Day34/main.py
+++ Day34/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import time, os
 
 Builder.load_file("frontend.kv")
 
@@ -16,7 +17,12 @@ class CameraScreen(Screen):
 		self.ids.camera.texture = None
 
 	def capture(self):
-		pass
+		current_time = time.strftime("%Y%m%d-%H%M%S")
+		if not os.path.isdir("files"):
+			os.mkdir("files")
+		file_path = f"files/{current_time}.png"
+		if self.ids.camera.play == True:
+			self.ids.camera.export_to_png(file_path)
 
 class ImageScreen(Screen):
 	pass
