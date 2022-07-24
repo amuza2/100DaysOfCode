2a04087a5319866da5ed4577848909e19d71bcfe
Mohammed Chami <mohamuza@gmail.com>
2022-07-24 20:47:18 +0100

    updated day 35

--- Day35/main.py
+++ Day35/main.py
@@ -6,4 +6,238 @@ root = Tk()
 root.geometry("1600x800+0+0")
 root.title("Restaurant Management System")
 
+text_input = StringVar()
+operator = ""
+
+tops = Frame(root, width=1600,bg="powder blue", relief=SUNKEN)
+tops.pack(side=TOP)
+
+f1 = Frame(root, width=800, height=700,pady=-100, relief=SUNKEN)
+f1.pack(side=LEFT)
+
+f2 = Frame(root, width=300, height=700, bg="powder blue",relief=SUNKEN)
+f2.pack(side=RIGHT)
+
+title_label = Label(tops, cursor="man", font=("arial", 50, "bold"), text="Restaurant Management System", fg="Steel Blue", bd=10)
+title_label.grid(row=0, column=0)
+
+local_time = time.asctime(time.localtime(time.time()))
+
+time_label = Label(tops, font=("arial", 20, "bold"), text=local_time,fg="Steel Blue", bd=10)
+time_label.grid(row=1, column=0)
+
+text_display = Entry(f2, font=("arial", 20, "bold"), textvariable=text_input,bd=30,insertwidth=4,
+					bg="powder blue", justify="right")
+text_display.grid(columnspan=4)
+
+ #Buttons
+def button_click(number):
+	global operator
+	operator += str(number)
+	text_input.set(operator)
+
+def button_clear():
+	global operator
+	operator = ""
+	text_input.set("")
+
+def button_equal():
+	global operator
+	sumit = str(eval(operator))
+	text_input.set(sumit)
+	operator=""
+
+def ref():
+	num = str(random.randint(10000, 99999))
+	rand.set(num)
+
+	cost_of_fries = 0.99
+	cost_of_drings = 1
+	cost_of_filet = 2.99
+	cost_of_burger = 2.87
+	cost_of_chicken = 2.89
+	cost_of_cheese = 2.69
+
+	cof = float(fries.get()) * cost_of_filet
+	cod = float(drinks.get()) * cost_of_drings
+	cofilet = float(filet.get()) * cost_of_filet
+	coburger = float(burger.get()) * cost_of_burger
+	cochicken = float(chicken.get()) * cost_of_chicken
+	cocheese = float(cheese.get()) * cost_of_cheese
+
+	cost_of_meal = f"£, {(cof+cod+cofilet+coburger+cochicken+cocheese)}"
+	pay_tax = (cof+cod+cofilet+coburger+cochicken+cocheese) * 0.2
+	service_charge = (cof+cod+cofilet+coburger+cochicken+cocheese) * 0.01
+	sub_total_gui = (cof+cod+cofilet+coburger+cochicken+cocheese)
+	total = (cof+cod+cofilet+coburger+cochicken+cocheese) + pay_tax + service_charge
+
+	tax_gui = f"£. {pay_tax}"
+	service_gui = f"£. {service_charge}"
+	sub_total_gui = f"£. {sub_total_gui}"
+	total_gui = f"£. {total}"
+
+	cost.set(cost_of_meal)
+	charge.set(service_gui)
+	state_tax.set(tax_gui)
+	sub_total.set(sub_total_gui)
+	total_cost.set(total_gui)
+
+def clear_button():
+	rand.set("")
+	fries.set("")
+	burger.set("")
+	filet.set("")
+	chicken.set("")
+	cheese.set("")
+	drinks.set("")
+	cost.set("")
+	charge.set("")
+	state_tax.set("")
+	sub_total.set("")
+	total_cost.set("")
+
+
+def exit():
+	root.destroy()
+
+#row 1 buttons
+button_7 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='7',bg="powder blue",command=lambda: button_click(7)).grid(row=2,column=0)
+button_8 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='8',bg="powder blue",command=lambda: button_click(8)).grid(row=2,column=1)
+button_9 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='9',bg="powder blue",command=lambda: button_click(9)).grid(row=2,column=2)
+button_addition = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='+',bg="powder blue",command=lambda: button_click('+')).grid(row=2,column=3)
+#row 2 buttons
+button_4 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='4',bg="powder blue",command=lambda: button_click(4)).grid(row=3,column=0)
+button_5 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='8',bg="powder blue",command=lambda: button_click(5)).grid(row=3,column=1)
+button_6 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='9',bg="powder blue",command=lambda: button_click(6)).grid(row=3,column=2)
+button_minus = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='-',bg="powder blue",command=lambda: button_click('-')).grid(row=3,column=3)
+
+#row 3 buttons
+button_1 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='1',bg="powder blue",command=lambda: button_click(1)).grid(row=4,column=0)
+button_2 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='2',bg="powder blue",command=lambda: button_click(2)).grid(row=4,column=1)
+button_3 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='3',bg="powder blue",command=lambda: button_click(3)).grid(row=4,column=2)
+button_multiply = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='*',bg="powder blue",command=lambda: button_click('*')).grid(row=4,column=3)
+#row 4 buttons
+button_0 = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='0',bg="powder blue",command=lambda: button_click(0)).grid(row=5,column=0)
+button_clear = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='C',bg="powder blue",command=button_clear).grid(row=5,column=1)
+button_equal = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='=',bg="powder blue",command=button_equal).grid(row=5,column=2)
+button_division = Button(f2, padx=16,pady=16,bd=8,fg="black",font=("arial",20,"bold"),
+				text='/',bg="powder blue",command=lambda: button_click('/')).grid(row=5,column=3)
+
+# reference label
+rand = StringVar()
+fries = StringVar()
+burger = StringVar()
+filet = StringVar()
+chicken = StringVar()
+cheese = StringVar()
+drinks = StringVar()
+cost = StringVar()
+charge = StringVar()
+state_tax = StringVar()
+sub_total = StringVar()
+total_cost = StringVar()
+
+
+
+#entries column 1
+label_reference = Label(f1, font=("arial",16,"bold"),text="Reference",bd=16)
+label_reference.grid(row=0,column=0)
+entry_reference = Entry(f1, font=("arial",16,"bold"),textvariable=rand,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_reference.grid(row=0,column=1)
+
+label_fries = Label(f1, font=("arial",16,"bold"),text="Large Fries",bd=16)
+label_fries.grid(row=1,column=0)
+entry_fries = Entry(f1, font=("arial",16,"bold"),textvariable=fries,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_fries.grid(row=1,column=1)
+
+label_burger = Label(f1, font=("arial",16,"bold"),text="Burger meal",bd=16)
+label_burger.grid(row=2, column=0)
+entry_burger = Entry(f1, font=("arial",16,"bold"),textvariable=burger,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_burger.grid(row=2,column=1)
+
+label_filet = Label(f1, font=("arial",16,"bold"),text="Filet o meal",bd=16)
+label_filet.grid(row=3, column=0)
+entry_filet = Entry(f1, font=("arial",16,"bold"),textvariable=filet,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_filet.grid(row=3,column=1)
+
+label_chicken = Label(f1, font=("arial",16,"bold"),text="Chicken meal",bd=16)
+label_chicken.grid(row=4, column=0)
+entry_chicken = Entry(f1, font=("arial",16,"bold"),textvariable=chicken,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_chicken.grid(row=4,column=1)
+
+label_cheese = Label(f1, font=("arial",16,"bold"),text="Cheese meal",bd=16)
+label_cheese.grid(row=5, column=0)
+entry_cheese = Entry(f1, font=("arial",16,"bold"),textvariable=cheese,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_cheese.grid(row=5,column=1)
+
+#entries column 2
+label_drink = Label(f1, font=("arial",16,"bold"),text="Drinks",bd=16)
+label_drink.grid(row=0,column=2)
+entry_drink = Entry(f1, font=("arial",16,"bold"),textvariable=drinks,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_drink.grid(row=0,column=3)
+
+
+label_cost = Label(f1, font=("arial",16,"bold"),text="Cost of meal",bd=16)
+label_cost.grid(row=1,column=2)
+entry_cost = Entry(f1, font=("arial",16,"bold"),textvariable=cost,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_cost.grid(row=1,column=3)
+
+label_charge = Label(f1, font=("arial",16,"bold"),text="Service charge",bd=16)
+label_charge.grid(row=2, column=2)
+entry_charge = Entry(f1, font=("arial",16,"bold"),textvariable=charge,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_charge.grid(row=2,column=3)
+
+label_tax = Label(f1, font=("arial",16,"bold"),text="State Tax",bd=16)
+label_tax.grid(row=3, column=2)
+entry_tax = Entry(f1, font=("arial",16,"bold"),textvariable=state_tax,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_tax.grid(row=3,column=3)
+
+label_sub = Label(f1, font=("arial",16,"bold"),text="Sub Total",bd=16)
+label_sub.grid(row=4, column=2)
+entry_sub = Entry(f1, font=("arial",16,"bold"),textvariable=sub_total,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_sub.grid(row=4,column=3)
+
+label_total_cost = Label(f1, font=("arial",16,"bold"),text="Total Cost",bd=16)
+label_total_cost.grid(row=5, column=2)
+entry_total_cost = Entry(f1, font=("arial",16,"bold"),textvariable=total_cost,bd=10,insertwidth=4,
+					bg="powder blue", justify="right")
+entry_total_cost.grid(row=5,column=3)
+
+#more buttons
+
+id_generate = Button(f1,pady=16,padx=16, bd=16, fg="black",font=("arial",16,"bold"),width=10,
+				text="Generate ID", bg="powder blue",command=ref).grid(row=7,column=1)
+clear_button = Button(f1,pady=16,padx=16, bd=16, fg="black",font=("arial",16,"bold"),width=10,
+				text="Clear", bg="powder blue",command=clear_button).grid(row=7,column=2)
+exit = Button(f1,pady=16,padx=16, bd=16, fg="black",font=("arial",16,"bold"),width=10,
+				text="Exit", bg="powder blue",command=exit).grid(row=7,column=3)
+
+
+
 root.mainloop()
