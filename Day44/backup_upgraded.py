f1cf04e67637c9859ec54e6fe26987c193bacc3b
Mohammed Chami <mohamuza@gmail.com>
2022-08-17 23:20:21 +0100

    updated day44

--- Day44/backup_upgraded.py
+++ Day44/backup_upgraded.py
@@ -1,13 +1,22 @@
-import PySimpleGUI as sg
-import os,shutil,zipfile
+"""
+	In this project I created a backup file
+	it's one of others projects to practice PySimpleGUI module
 
-col_1 = [
-		[sg.CB("Compress",key="-CBCOM-")],
-		]
+"""
+import os
+import shutil
+import zipfile
+import PySimpleGUI as sg
+
+
+col_1 = [[sg.CB("Compress",key="-CBCOM-")]]
 
 layout = [
-		[sg.T("Choose Source Folder:",size=(19,1)),sg.I(key="-SOURCEFOLDER-",enable_events=True),sg.FolderBrowse()],
-		[sg.T("Choose Destination Folder:",size=(19,1)),sg.I(key="-DESTINATIONFOLDER-"),sg.FolderBrowse()],
+		[sg.T("Choose Source Folder:",size=(19,1)),
+			sg.I(key="-SOURCEFOLDER-",enable_events=True),
+			sg.FolderBrowse()],
+		[sg.T("Choose Destination Folder:",size=(19,1)),
+			sg.I(key="-DESTINATIONFOLDER-"),sg.FolderBrowse()],
 		[sg.T("File name:",size=(19,1)), sg.I(key="-FILENAME-")],
 		[sg.T("File List:")],
 		[sg.Listbox(values='',key="-LISTBOX-", size=(50,15)),sg.Col(col_1)],
@@ -16,16 +25,14 @@ layout = [
 
 window = sg.Window("Backup tool", layout,size=(600,450))
 while True:
-	event, values = window.read()
-	
+	event,values = window.read()
 	if event in ("Exit", sg.WIN_CLOSED):
 		window.close()
 		break
-	elif event == "-SOURCEFOLDER-":
+	if event == "-SOURCEFOLDER-":
 		window["-LISTBOX-"].update('')
 		fname = os.listdir(values["-SOURCEFOLDER-"])
 		window["-LISTBOX-"].update(fname)
-		
 	elif event == "Backup":
 		if not values["-SOURCEFOLDER-"]:
 			sg.popup("Select Source Folder")
@@ -39,18 +46,13 @@ while True:
 			file_name = values["-FILENAME-"]
 			folder_name = os.path.basename(values["-SOURCEFOLDER-"])
 			file_path = os.path.join(os.getcwd(),f"{file_name}.zip")
-			
-			compress = False
+			COMPRESS = False
 			if values["-CBCOM-"]:
-				compress = zipfile.ZIP_DEFLATED
-			with zipfile.ZipFile(f"{file_name}.zip", 'w',compression=compress) as f:
+				COMPRESS = zipfile.ZIP_DEFLATED
+			with zipfile.ZipFile(f"{file_name}.zip", 'w',compression=COMPRESS) as f:
 				for image_name in fname:
 					file = os.path.join(source_path,image_name)
 					f.write(file,arcname=os.path.join(f"{folder_name}/", image_name))
 			shutil.move(file_path, destination_path)
 			print("Zip files Created and moved to destination")
-
-
-
-
 	print(event, values)
