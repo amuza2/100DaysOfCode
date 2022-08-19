145af009a335b0f8791edb597eb33e147e4b809a
Mohammed Chami <mohamuza@gmail.com>
2022-08-19 18:49:24 +0100

    updated youtube downloader file

--- Day45/youtube_downloader.py
+++ Day45/youtube_downloader.py
@@ -1,17 +1,11 @@
 """
-	This is Youtube Downloader project
-	used the youtube_dl module
-	For downloading Youtube video as mp3
-
+	convering youtube videos into mp3 files
 """
-
 import youtube_dl
-import threading
 
 
-def run():
-    video_urls = ["https://www.youtube.com/watch?v=pmlNSa1szEA",
-    			"https://www.youtube.com/watch?v=voICYzatU5c"]
+def run(video_urls):
+    """ this funtion responssible for downloading and convering"""
     
     for video_url in video_urls:
 	    video_info = youtube_dl.YoutubeDL().extract_info(
@@ -27,12 +21,3 @@ def run():
 	        ydl.download([video_info['webpage_url']])
     
     print(f"Download complete... {filename}")
-
-if __name__=='__main__':
-	run()
-
-
-
-
-
-
