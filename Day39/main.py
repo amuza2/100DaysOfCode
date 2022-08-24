e3facc200056dcf4f1f03c1056a6912e37a3d756
Mohammed Chami <mohamuza@gmail.com>
2022-08-24 20:52:34 +0100

    updated day39

--- Day39/main.py
+++ Day39/main.py
@@ -1,21 +1,23 @@
 from docxtpl import DocxTemplate
-import os, csv
+import os
+import csv
 
 with open("cars.csv") as f:
 	data = csv.reader(f)
 	data.__next__()
 	counter = 1
-	for i in data:
+	for column in data:
 		doc = DocxTemplate("cars_info.docx")
 		context = {
-				"date": i[0],
-				"name": i[1],
-				"price": i[2],
-				"gas_consumption": i[3],
-				"miles": i[4],
+				"date": column[0],
+				"name": column[1],
+				"price": column[2],
+				"gas_consumption": column[3],
+				"miles": column[4],
 				}
 		doc.render(context)
 		if not os.path.isdir("cars"):
 			os.mkdir("cars")
 		doc.save(f"cars/cars_details{counter}.docx")
 		counter+=1
+		
