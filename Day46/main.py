0a401e560fe795ec8ace99dcd309989dd231ecbc
Mohammed Chami <mohamuza@gmail.com>
2022-08-26 16:40:36 +0100

    updated day 46

--- Day46/main.py
+++ Day46/main.py
@@ -5,18 +5,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 class HomePage(MethodView):
+
 	def get(self):
 		return render_template("index.html")
 
 
 class BillFormPage(MethodView):
+
 	def get(self):
 		bill_form = BillForm()
 		return render_template("bill_form_page.htm", billform=bill_form)
 
 
 class ResultPage(MethodView):
-
+	
 	def post(self):
 		billform = BillForm(request.form)
 		amount = billform.amount.data
@@ -24,6 +26,7 @@ class ResultPage(MethodView):
 
 
 class BillForm(Form):
+	
 	amount = StringField("Bill Amount: ")
 	period = StringField("Bill Period: ")
 	name_1 = StringField("Name: ")
@@ -37,4 +40,7 @@ class BillForm(Form):
 app.add_url_rule('/', view_func=HomePage.as_view("home_page"))
 app.add_url_rule("/bill",view_func=BillFormPage.as_view("bill_form_page"))
 app.add_url_rule("/results", view_func=ResultPage.as_view("results_page"))
-app.run(debug=True)
+if "__main__" == __name__:
+	app.jinja_env.auto_reload = True
+	app.config['TEMPLATES_AUTO_RELOAD'] = True
+	app.run(debug=True, host='0.0.0.0')
