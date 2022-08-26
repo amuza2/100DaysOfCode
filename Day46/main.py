3ebd8de1fe3d9a52d09260e614a613f91b26eacd
Mohammed Chami <mohamuza@gmail.com>
2022-08-26 11:14:31 +0100

    updated day 46

--- Day46/main.py
+++ Day46/main.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
-from wtforms import Form, StringField
-from flask import Flask, render_template
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,20 +8,33 @@ class HomePage(MethodView):
 	def get(self):
 		return render_template("index.html")
 
+
 class BillFormPage(MethodView):
 	def get(self):
-		return render_template("bill_form_page.htm")
+		bill_form = BillForm()
+		return render_template("bill_form_page.htm", billform=bill_form)
 
 
 class ResultPage(MethodView):
-	pass
+
+	def post(self):
+		billform = BillForm(request.form)
+		amount = billform.amount.data
+		return amount
 
 
 class BillForm(Form):
-	amount = 
+	amount = StringField("Bill Amount: ")
+	period = StringField("Bill Period: ")
+	name_1 = StringField("Name: ")
+	days_in_house_1 = StringField("Days in the house: ")
+	name_2 = StringField("Name: ")
+	days_in_house_2 = StringField("Days in the house: ")
+
+	button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view("home_page"))
 app.add_url_rule("/bill",view_func=BillFormPage.as_view("bill_form_page"))
-
+app.add_url_rule("/results", view_func=ResultPage.as_view("results_page"))
 app.run(debug=True)
