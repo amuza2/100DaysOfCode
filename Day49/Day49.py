8efb9458ac23f37b109e446988316ebb7962f4e8
Mohammed Chami <mohamuza@gmail.com>
2022-09-02 23:16:12 +0100

    updated day 49

--- Day49/Day49.py
+++ Day49/Day49.py
@@ -1,15 +1,25 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAME_PER_SECOND = 30
 
+BALL_WIDTH_HEIGHT = 100
+MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
+MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+
+ballImage = pygame.image.load("images/ball.png")
+
+ballX = rendom.randrange(MAX_WIDTH)
+ballY = random.randrange(MAX_HEIGHT)
 
 
 while True:
@@ -19,7 +29,9 @@ while True:
 			pygame.quit()
 			sys.exit()
 
-	window.fill(BLACK)
+
+	window.blit(ballImage, (100,200))
+	# window.fill(BLACK)
 	pygame.display.update()
 	clock.tick(FRAME_PER_SECOND)
 
