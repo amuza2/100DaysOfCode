05d5f904887fce60dd1ce45790d7c5cbf3ada48c
Mohammed Chami <mohamuza@gmail.com>
2022-09-03 21:37:05 +0100

    updated day 49 created a bouncing ball

--- Day49/Day49.py
+++ Day49/Day49.py
@@ -3,22 +3,17 @@ from pygame.locals import *
 import sys
 import random
 
-
+#screen
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAME_PER_SECOND = 30
+N_PIXELS_PER_FRAME = 7
+BALL_WIDTH_HEIGHT = 100
 
 #ball
-BALL_WIDTH_HEIGHT = 100
 MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
 MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
-
-#target
-TARGET_X = 400
-TARGET_Y = 320
-TARGET_WIDTH_HEIGHT = 120
-N_PIXELS_TO_MOVE = 3
 
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
@@ -26,15 +21,13 @@ clock = pygame.time.Clock()
 
 #load images
 ballImage = pygame.image.load("images/ball.png")
-targetImage = pygame.image.load("images/target.jpg")
 
 #ball position
 ballX = random.randrange(MAX_WIDTH)
 ballY = random.randrange(MAX_HEIGHT)
-# ballRect = pygame.Rect(ballX, ballY, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
-# target position
-targetRect = pygame.Rect(TARGET_X, TARGET_Y, TARGET_WIDTH_HEIGHT, TARGET_WIDTH_HEIGHT) 
-
+xSpeed = N_PIXELS_PER_FRAME
+ySpeed = N_PIXELS_PER_FRAME
+random.seed(3)
 while True:
 
 	for event in pygame.event.get():
@@ -42,24 +35,16 @@ while True:
 			pygame.quit()
 			sys.exit()
 
-	keyPressedTuple = pygame.key.get_pressed()
-	if keyPressedTuple[pygame.K_LEFT]:
-		ballX -= N_PIXELS_TO_MOVE
-	if keyPressedTuple[pygame.K_RIGHT]:
-		ballX += N_PIXELS_TO_MOVE
-	if keyPressedTuple[pygame.K_UP]:
-		ballY -= N_PIXELS_TO_MOVE
-	if keyPressedTuple[pygame.K_DOWN]:
-		ballY += N_PIXELS_TO_MOVE
+	if (ballX<0) or (ballX>=MAX_WIDTH):
+		xSpeed = -xSpeed
+	if (ballY<0) or (ballY>=MAX_HEIGHT):
+		ySpeed = -ySpeed
 
-	ballRect = pygame.Rect(ballX, ballY, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
-	if ballRect.colliderect(targetRect):
-		print("Ball is touching the target")
-
+	ballX += xSpeed
+	ballY += ySpeed
 		
 
 	window.fill(BLACK)
-	window.blit(targetImage, (TARGET_X,TARGET_Y))
 	window.blit(ballImage, (ballX,ballY))
 	pygame.display.update()
 	clock.tick(FRAME_PER_SECOND)
