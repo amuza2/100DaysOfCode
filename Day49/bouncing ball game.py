be0f23157fd3f0e09b5d78feec2a282f4c566540
Mohammed Chami <mohamuza@gmail.com>
2022-09-04 02:35:45 +0100

    updated day 49 added sounds and renamed file

--- Day49/bouncing ball game.py
+++ Day49/bouncing ball game.py
@@ -21,10 +21,17 @@ clock = pygame.time.Clock()
 
 #load images
 ballImage = pygame.image.load("images/ball.png")
+ballRect = ballImage.get_rect()
+bounce_sound = pygame.mixer.Sound("sounds/boing.wav")
+pygame.mixer.music.load("sounds/background.mp3")
+pygame.mixer.music.play(-1,0.0)
 
-#ball position
-ballX = random.randrange(MAX_WIDTH)
-ballY = random.randrange(MAX_HEIGHT)
+#ball spawn position
+MAX_WIDTH = WINDOW_WIDTH - ballRect.width
+MAX_HEIGHT = WINDOW_HEIGHT - ballRect.height
+ballRect.left = random.randrange(MAX_WIDTH)
+ballRect.top = random.randrange(MAX_HEIGHT)
+
 xSpeed = N_PIXELS_PER_FRAME
 ySpeed = N_PIXELS_PER_FRAME
 random.seed(3)
@@ -35,17 +42,19 @@ while True:
 			pygame.quit()
 			sys.exit()
 
-	if (ballX<0) or (ballX>=MAX_WIDTH):
+	if (ballRect.left<0) or (ballRect.right>=WINDOW_WIDTH):
+		bounce_sound.play()
 		xSpeed = -xSpeed
-	if (ballY<0) or (ballY>=MAX_HEIGHT):
+	if (ballRect.top<0) or (ballRect.bottom>=WINDOW_HEIGHT):
+		bounce_sound.play()
 		ySpeed = -ySpeed
 
-	ballX += xSpeed
-	ballY += ySpeed
+	ballRect.left += xSpeed
+	ballRect.top += ySpeed
 		
 
 	window.fill(BLACK)
-	window.blit(ballImage, (ballX,ballY))
+	window.blit(ballImage, ballRect)
 	pygame.display.update()
 	clock.tick(FRAME_PER_SECOND)
 
