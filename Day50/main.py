3b7204d8891032a06ba8dd92720c164d51a528e1
Mohammed Chami <mohamuza@gmail.com>
2022-09-05 00:19:51 +0100

    update day 50

--- Day50/main.py
+++ Day50/main.py
@@ -13,9 +13,16 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-buttonUp= "images/buttonUp.png"
-buttonDown = "images/buttonDown.png"
-oButton = SimpleButton(window, (150,30), buttonUp, buttonDown)
+buttonAUp= "images/buttonAUp.png"
+buttonADown = "images/buttonADown.png"
+buttonBUp= "images/buttonBUp.png"
+buttonBDown = "images/buttonBDown.png"
+buttonCUp= "images/buttonCUp.png"
+buttonCDown = "images/buttonCDown.png"
+
+oButtonA = SimpleButton(window, (25,30), buttonAUp, buttonADown)
+oButtonB = SimpleButton(window, (150,30), buttonBUp, buttonBDown)
+oButtonC = SimpleButton(window, (275,30), buttonCUp, buttonCDown)
 
 while True:
 
@@ -24,8 +31,18 @@ while True:
 			pygame.quit()
 			sys.exit()
 
-	oButton.handleEvent(event)
+	if oButtonA.handleEvent(event):
+		print("You pressed button A")
+	elif oButtonB.handleEvent(event):
+		print("You pressed button B")
+	elif oButtonC.handleEvent(event):
+		print("You pressed button C")
+
 	window.fill(BLACK)
-	oButton.draw()
+
+	oButtonA.draw()
+	oButtonB.draw()
+	oButtonC.draw()
+	
 	pygame.display.update()
 	clock.tick(FRAME_PER_SECONDS)
