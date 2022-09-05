c066b4678bef251d336c766359fcf2a5a9382b76
Mohammed Chami <mohamuza@gmail.com>
2022-09-05 17:24:32 +0100

    update day 50

--- Day50/main_ball_text_and_button.py
+++ Day50/main_ball_text_and_button.py
@@ -18,7 +18,7 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
 oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
-oFrameCountLabel = SimpleText(window, (60, 20), "Programe has run through this many loop:", WHITE, textColor)
+oFrameCountLabel = SimpleText(window, (60, 20), "Programe has run through this many loop:", WHITE)
 oFrameCountDisplay = SimpleText(window, (500, 20), '', WHITE)
 oRestartButton = SimpleButton(window, (280, 60), "images/restartUp.png", "images/restartDown.png")
 frameCounter = 0
@@ -29,3 +29,20 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+
+
+		if oRestartButton.handleEvent(event):
+			oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+			frameCounter = 0
+
+
+	oBall.update()
+	frameCounter += 1
+	oFrameCountDisplay.setValue(str(frameCounter))
+	window.fill(BLACK)
+	oBall.draw()
+	oFrameCountLabel.draw()
+	oFrameCountDisplay.draw()
+	oRestartButton.draw()
+	pygame.display.update()
+	clock.tick(FRAME_PER_SECONDS)
