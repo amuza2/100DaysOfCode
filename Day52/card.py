8df62700a380674d51851b08e56a9c952b291d5e
Mohammed Chami <mohamuza@gmail.com>
2022-09-15 16:51:05 +0100

    updated day 52

--- Day52/card.py
+++ Day52/card.py
@@ -4,7 +4,7 @@ import pygwidgets
 
 
 class Card():
-	BACK_OF_IMAGE = pygame.image.load("images/BackOfCard.png")
+	BACK_OF_CARD_IMAGE = pygame.image.load("images/BackOfCard.png")
 
 	def __init__(self, window, rank, suit, value):
 		self.window = window
@@ -14,3 +14,33 @@ class Card():
 		self.cardName = rank + " of " + suit
 
 		fileName = "images/" + self.cardName + ".png"
+
+		self.images = pygwidgets.ImageCollection(window, (0,0), {"front":fileName,
+																"back": Card.BACK_OF_CARD_IMAGE}, "back")
+		def conceal(self):
+			self.images.replace("back")
+
+		def reveal(self):
+			self.images.replace("front")
+
+		def getName(self):
+			return self.cardName
+
+		def getValue(self):
+			return self.value
+
+		def getSuit(self):
+			return self.suit
+
+		def getRank(self):
+			return self.rank
+
+		def setLoc(self, loc):
+			self.images.setLoc(loc)
+
+		def getLoc(self):
+			loc = self.images.getLoc()
+			return loc
+
+		def draw(self):
+			self.images.draw()
---
+++ Day52/deck.py
@@ -0,0 +1,35 @@
+
+import random
+from card import *
+
+
+class Deck():
+	SUIT_TUPLE = ("Diamonds", "Clubs", "Hearts", "Spades")
+	STANDARD_DICT = {"Ace":1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,"Jack":11,"Queen":12,"King":13}
+
+	def __init__(self, window, rankValueDick=STANDARD_DICT):
+		self.startingDeckList = []
+		self.playingDeckList = []
+		for suit in Deck.SUIT_TUPLE:
+			for rank, value in rankValueDick.items():
+				oCard = Card(window, rank, suit, value)
+				self.startingDeckList.append(oCard)
+
+			self.shuffle()
+
+	def shuffle(self):
+		self.playingDeckList = self.startingDeckList.copy()
+		for oCard in self.playingDeckList:
+			oCard.conceal()
+		random.shuffle(self.playingDeckList)
+
+	def getCard(self):
+		if len(self.playingDeckList) == 0:
+			raise IndexError("No more Cards")
+		oCard = self.playingDeckList.pop()
+		return oCard
+
+	def returnCardToDeck(self, oCard):
+		self.playingDeckList.insert(0, oCard)
+
+
