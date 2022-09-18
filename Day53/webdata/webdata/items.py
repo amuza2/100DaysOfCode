44f58b4b3456d5709a8ad1a8c6819b2e48ec7415
Mohammed Chami <mohamuza@gmail.com>
2022-09-18 20:33:12 +0100

    updated day 53

--- Day53/webdata/webdata/items.py
+++ Day53/webdata/webdata/items.py
@@ -4,9 +4,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst
+from urllib.parse import urljoin
 
+def remove_currency(value):
+    return value.replace('£', '')
+
+def join_url(value):
+    return urljoin("https://books.toscrape.com/", value)
 
 class WebdataItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field(output_processor = TakeFirst())
+    price = scrapy.Field(input_processor = MapCompose(remove_currency),
+                         output_processor=TakeFirst())
+    url = scrapy.Field(input_processor = MapCompose(join_url),
+                        output_processor=TakeFirst())
--- Day53/webdata/webdata/spiders/main.py
+++ Day53/webdata/webdata/spiders/main.py
@@ -1,12 +1,20 @@
 import scrapy
-
+from webdata.items import WebdataItem
+from urllib.parse import urljoin
+from scrapy.loader import ItemLoader
 
 class MainSpider(scrapy.Spider):
     name = 'main'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+    start_urls = ['https://books.toscrape.com/']
 
     def parse(self, response):
         print("---- Scraping ----")
 
-        print(response.xpath("//h3/a/@href").get())
+        for box in response.xpath("//article[@class='product_pod']"):
+            data = ItemLoader(item=WebdataItem(), selector=box)
+            data.add_xpath("url", ".//h3/a/@href")
+            data.add_xpath("title", ".//h3/a/@title")
+            data.add_xpath("price", ".//p[@class='price_color']/text()")
+
+            yield data.load_item()
