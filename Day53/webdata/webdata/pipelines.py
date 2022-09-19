61da989b0c1eaaae81b9de0ed34e4217a90986af
Mohammed Chami <mohamuza@gmail.com>
2022-09-19 14:40:45 +0100

    update save to excel file

--- Day53/webdata/webdata/pipelines.py
+++ Day53/webdata/webdata/pipelines.py
@@ -6,8 +6,21 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from openpyxl import Workbook
 
 
 class WebdataPipeline:
+    cols = ["Name", "price", "url"]
+
+    def open_spider(self, spider):
+        self.workbook = Workbook()
+        self.sheet = self.workbook.active
+        self.sheet.title = "ebooks"
+        self.sheet.append(self.cols)
+
     def process_item(self, item, spider):
+        self.sheet.append([item["title"], item["price"], item["url"]])
         return item
+
+    def close_spider(self, spider):
+        self.workbook.save("ebooks.xlsx")
