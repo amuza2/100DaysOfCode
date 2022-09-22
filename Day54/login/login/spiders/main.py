74a4ccb6c5f955c6a0e6bcebbe096c6405d65f4e
Mohammed Chami <mohamuza@gmail.com>
2022-09-22 01:22:46 +0100

    updated day 54

--- Day54/login/login/spiders/main.py
+++ Day54/login/login/spiders/main.py
@@ -4,7 +4,15 @@ import scrapy
 class MainSpider(scrapy.Spider):
     name = 'main'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/login']
+
 
     def parse(self, response):
-        pass
+        url = "http://quotes.toscrape.com/login"
+        csrf = response.xpath("//form/input/@value").get()
+        yield scrapy.FormRequest(url=url, formdata={"username":"amuza","password":"mypassword","csrf_token":csrf}, callback=self.loged_in)
+        
+    def loged_in(self, response):
+        print("Login done!")
+        print(response.xpath("//div[@class='quote']/span[@class='text']/text()").get())
+        
