64784199960f9342e54e0da241f2a22f5f4ec10d
Mohammed Chami <mohamuza@gmail.com>
2022-10-02 12:25:50 +0100

    updated day 45

--- Day45/youtube_downloader.py
+++ Day45/youtube_downloader.py
@@ -1,31 +1,32 @@
-"""
-	convering youtube videos into mp3 files
-"""
 from youtube_dl import YoutubeDL
 
+
 class ConvertToMp3():
-	""" this class has method to convert mp4 youtube vids into mp3 """
 
-	def __init__(self):
-		pass
+    def get_video_title(self, url):
+        """ get the video title
 
-	def get_video_title(self, url):
-		"""  get video title 
-			 url: youtube url
-		"""
-		video_info = YoutubeDL().extract_info(url=url,download=False)
-		video_title = video_info.get('title', None)
-		return video_title
+        Args:
+            url (string): Youtube url
 
-	def run(self, video_url):
-		""" This method take video_url, get video info and download file 
-			video_url: youtube url
-		"""
-		video_info = YoutubeDL().extract_info(url=video_url,download=False)
-		video_title = video_info.get("title", None)
-		filename = str(video_title) + ".mp3"
-		options={'format':'bestaudio/best', 'keepvideo':False, 'outtmpl':filename}
-		
-		with YoutubeDL(options) as ydl:
-			ydl.download([video_info['webpage_url']])
+        Returns:
+            string: Youtube video title
+        """
+        video_info = YoutubeDL().extract_info(url=url, download=False)
+        video_title = video_info.get('title', None)
+        return video_title
 
+    def start_download(self, url):
+        """Download file as mp3 format
+
+        Args:
+            url (string): Youtube url
+        """
+        video_info = YoutubeDL().extract_info(url=url, download=False)
+        video_title = video_info.get("title", None)
+        filename = str(video_title) + ".mp3"
+        options = {'format': 'bestaudio/best',
+                   'keepvideo': False, 'outtmpl': filename}
+
+        with YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
