a1edda41bef233e63c8e8736b11391e6dd8cf89f
Mohammed Chami <mohamuza@gmail.com>
2022-10-31 17:25:53 +0100

    finish this project

--- Day58/sports/sports/spiders/main.py
+++ Day58/sports/sports/spiders/main.py
@@ -4,29 +4,34 @@ import scrapy
 class MainSpider(scrapy.Spider):
     name = 'main'
     allowed_domains = ['espn.com']
-    start_urls = ['https://www.espn.com/soccer/table/_/league/eng.1']
+    start_urls = [
+        'https://www.espn.com/soccer/standings/_/league/uefa.champions']
 
     def parse(self, response):
         gp, w, d, l, f, a, gd, p = [], [], [], [], [], [], [], []
-
+        groups = response.css(".Table--fixed-left .dib::text").getall()
         teams = response.css(".Table__TD .hide-mobile a::text").getall()
-        # header = response.css(
-        # ".Table__Scroller .Table__header-group.Table__THEAD a::text").getall()
         table = response.css(
             ".Table__Scroller .Table__TBODY .Table__TR.Table__TR--sm.Table__even")
         for row in table:
             values = row.css("td span::text").getall()
-            gp.append(values[0])
-            w.append(values[1])
-            d.append(values[2])
-            l.append(values[3])
-            f.append(values[4])
-            a.append(values[5])
-            gd.append(values[6])
-            p.append(values[7])
-
+            try:
+                gp.append(values[0])
+                w.append(values[1])
+                d.append(values[2])
+                l.append(values[3])
+                f.append(values[4])
+                a.append(values[5])
+                gd.append(values[6])
+                p.append(values[7])
+            except:
+                continue
+        current_group = 0
         for c in range(len(teams)):
+            if (c > 0) and (c % 4 == 0):
+                current_group += 1
             yield{
+                "Groups": groups[current_group],
                 "TEAMS": teams[c],
                 "GP": gp[c],
                 "w": w[c],
