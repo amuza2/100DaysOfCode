6c0294e30929441e740f5cf404de51f124c3808a
Mohammed Chami <mohamuza@gmail.com>
2022-12-22 20:07:38 +0100

    started learning html and css

---
+++ Day62/website/file.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learningcsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Mohammed");
+            Console.ReadLine();
+        }
+    }
+}
