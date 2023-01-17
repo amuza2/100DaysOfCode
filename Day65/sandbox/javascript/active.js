520aae69d171159a2eb6799afed06d6230a3e9da
Mohammed Chami <mohamuza@gmail.com>
2023-01-17 15:43:22 +0100

    added javascript

---
+++ Day65/sandbox/javascript/active.js
@@ -0,0 +1,16 @@
+/*alert("USER NOTICE "  +"\n"
++"\n"
++"New users are not allowed to work " +"\n"
++"with that feature.");
+*/	 
+
+/*function myFunc(person){
+    let person = prompt("Please enter your name", "Harry Potter");
+    document.getElementById("demo").innerHTML = "Hello " + person + "! How are you today?";
+}*/
+
+let value1 = prompt("Please enter first value");
+let value2 = prompt("Please enter second value");
+let value3 = prompt("Please enter third value");
+alert("the sum of these values is: " + (Number(value1) + Number(value2) + Number(value3)))
+ 
