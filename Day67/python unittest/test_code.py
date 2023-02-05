aa55dbc13a23be615d18176c139cb3c72f3a7987
Mohammed Chami <mohamuza@gmail.com>
2023-02-05 20:41:10 +0100

    added python unitest and updated sandbox03

--- Day67/python unittest/test_code.py
+++ Day67/python unittest/test_code.py
@@ -1,12 +1,30 @@
 import unittest
-import main
+import calc
 
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = main.add(10, 5)
-        return self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+    def test_minus(self):
+        self.assertEqual(calc.minus(10, 5), 5)
+        self.assertEqual(calc.minus(-1, 1), -2)
+        self.assertEqual(calc.minus(-1, -1), 0)
+
+    def test_multi(self):
+        self.assertEqual(calc.multi(10, 5), 50)
+        self.assertEqual(calc.multi(-1, 1), -1)
+        self.assertEqual(calc.multi(-1, -1), 1)
+
+    def test_div(self):
+        self.assertEqual(calc.div(10, 5), 2)
+        self.assertEqual(calc.div(-1, 1), -1)
+        self.assertEqual(calc.div(-1, -1), 1)
+
+        self.assertRaises(ValueError, calc.div, 10, 2)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
