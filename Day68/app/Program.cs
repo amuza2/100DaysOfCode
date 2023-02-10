743164f907b25419e25e105ee62eee0418bb810b
Mohammed Chami <mohamuza@gmail.com>
2023-02-11 00:04:27 +0100

    codewars

--- Day68/app/Program.cs
+++ Day68/app/Program.cs
@@ -2,14 +2,20 @@
 static int[] RoundUp(int number, int[] list)
   {
     int[] arr = new int[2];
+    if (list.Length == 0) return new int[0];
+    bool single = true; 
     arr[0] = list[0];
     foreach(int i in list)
       {
         if (i == arr[0]) continue;
-        if (Math.Abs(number - i) < Math.Abs(number - list[0])) list[0] = i;
-        else if (Math.Abs(number - i) == Math.Abs(number - list[0])) list[1] = i;
+        if (Math.Abs(number - i) < Math.Abs(number - arr[0])) arr[0] = i;
+        else if (Math.Abs(number - i) == Math.Abs(number - arr[0])) {arr[1] = i; single=false;}
       }
+    if (single)
+    {
+      //int[] arr1 = {arr[0]};
+      return arr[0].ToArray();
+    }
     return arr;
   }
-int[] r = RoundUp(10, new int[] {1,2,3,4,6,12});
-foreach(int i in r) Console.WriteLine(i);
+RoundUp(1, new int[] {-32,12,8,-10,-2});
