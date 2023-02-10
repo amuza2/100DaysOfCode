e3ce305520c481e177d29b41a59f59fba7008b7f
Mohammed Chami <mohamuza@gmail.com>
2023-02-10 17:34:54 +0100

    codewars challenges

---
+++ Day68/app/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int[] RoundUp(int number, int[] list)
+  {
+    int[] arr = new int[2];
+    arr[0] = list[0];
+    foreach(int i in list)
+      {
+        if (i == arr[0]) continue;
+        if (Math.Abs(number - i) < Math.Abs(number - list[0])) list[0] = i;
+        else if (Math.Abs(number - i) == Math.Abs(number - list[0])) list[1] = i;
+      }
+    return arr;
+  }
+int[] r = RoundUp(10, new int[] {1,2,3,4,6,12});
+foreach(int i in r) Console.WriteLine(i);
