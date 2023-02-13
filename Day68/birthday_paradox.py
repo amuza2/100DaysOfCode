d36db56a11d62eba319fe93c7929cda497d7c98f
Mohammed Chami <mohamuza@gmail.com>
2023-02-13 05:10:07 +0100

    update birthday paradox

--- Day68/birthday_paradox.py
+++ Day68/birthday_paradox.py
@@ -1,17 +1,26 @@
-import random, datetime
+import random
+import datetime
 
 number_of_birthdays = 23
-
 dates = []
-
-
+matches = 0
+birthday = []
 for i in range(100_000):
-	if i % 5000 == 0:
-		print("reached",i)
-	for j in range(number_of_birthdays):
-		day = random.randint(1, 28)
-		month = random.randint(1, 12)
-		dates.append(datetime.date(2023, month, day))
+    if i % 5000 == 0:
+        print("reached", i)
+    for j in range(number_of_birthdays):
+        day = random.randint(1, 28)
+        month = random.randint(1, 12)
+        dates.append(datetime.date(2023, month, day))
+        birthday.append(dates[j].strftime("%b %d"))
+
+    for z in range(number_of_birthdays):
+        temp = birthday[i]
+        birthday[i] = '*'
+        if temp in birthday:
+            matches += 1
+        birthday[i] = temp
 
 for i in range(number_of_birthdays):
-	print(dates[i].strftime("%b %d"), end=", ")
+    print(birthday[i], end=", ")
+print("matches:", matches)
