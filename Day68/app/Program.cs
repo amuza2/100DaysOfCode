96990e317be04cbf9083b64b6f0b0bdc29c6fb37
Mohammed Chami <mohamuza@gmail.com>
2023-03-06 10:08:46 +0100

    updated

--- Day68/app/Program.cs
+++ Day68/app/Program.cs
@@ -1,28 +1,34 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace Code_Coach_Challenge
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            const string filename = "Testfile.txt";
+            //const string filename = "text.txt";
+            //using(StreamWriter sw = File.CreateText(filename)){
+            //    sw.WriteLine("Nadjat Ghofran");
+            //}
 
-            // using (StreamWriter sw = File.CreateText(filename)){
-            //     sw.WriteLine("This is a text file.");
+            // StreamWriter file = new StreamWriter("numbers.txt");
+            // int[] num = {1,2,3,4,5};
+            // for(int i = 0; i<num.Length; i++){
+            //     file.WriteLine(num[i]);
             // }
-            File.AppendAllText(filename, "This is a new text.");
-            // using (StreamWriter sw = File.AppendText(filename)){
+            // file.Close();
 
-            // }
-
-
-            string content;
-            content = File.ReadAllText(filename);
-            Console.WriteLine(content);
+            StreamReader file = new StreamReader("numbers.txt");
+            int num;
+            while(!file.EndOfStream){
+                num = int.Parse(file.ReadLine());
+                Console.WriteLine(num);
+            }
+            //file.Close();
 
         }
     }
-
 }
