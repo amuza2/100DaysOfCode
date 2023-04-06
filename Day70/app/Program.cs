ba985c1e225be8c8758e14338db64a624787c2c6
Mohammed Chami <mohamuza@gmail.com>
2023-04-06 02:35:12 +0100

    learned to return multiple values from a finction

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+(int x, int y) tup = (5, 12);
+var tup2 = ("this is a string", 12.5f);
+static (int a, int b) myFunction(int a, int b)
+{
+    return (a + b, a * b);
+}
+
+(int, int) result = myFunction(6, 12);
+Console.WriteLine($"The first value is {result.Item1}\nThe second value is {result.Item2}");
