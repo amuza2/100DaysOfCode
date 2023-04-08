074fd9bc2d56fc0728ac340980265853f82fc8fe
Mohammed Chami <mohamuza@gmail.com>
2023-04-08 14:44:34 +0100

    updated

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -1,21 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-void Palindrome(string value){
-    bool pal = true;
-    int odd = 0;
-    int len = value.Length; 
-    if(len % 2 == 1) odd = 1;
-    for(int i = 0; i < len; i++){
-        if(value[i] != value[len - 1 - i]){
-            Console.WriteLine($"{value[i]} at index {i} != {value[len - 1]} at index {len - i - 1}");
-            pal = false;
-            break;
-        }
-        if(i == (len / 2) + odd) break;
+namespace selfLearning;
+class Person
+{
+    public int Age {get; set;}
+}    
+class program
+{
+    static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.Age = 15;
+        Console.WriteLine(p1.Age);
     }
-    if(pal == false) Console.WriteLine("It's not!");
-    else Console.WriteLine("It is!");
-    Console.WriteLine("----------------------");
 }
-
-Palindrome("mohammed");
