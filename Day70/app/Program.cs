b3f0c76d8ad9b6f133371631a9cc62bcf6b707ae
Mohammed Chami <mohamuza@gmail.com>
2023-04-09 21:17:03 +0100

    update

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -1,15 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace selfLearning;
-class Person
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
 {
-    public int Age {get; set;}
-}    
-class program
+class Program
 {
+    public void Palindrome(string value)
+    {
+        bool pal = true;
+        int odd = 0;
+        int len = value.Length;
+
+        if(len % 2 == 1) odd = 1;
+        for(int i = 0; i < len; i++)
+        {
+            if(value[i] != value[len - 1 - i])
+            {
+                Console.WriteLine($"{value[i]} at index {i} != {value[len - 1]} at index {len - i - 1}");
+                pal = false;
+                break;
+            }
+            if(i == (len / 2) + odd) break;
+        }
+        if(pal == false) Console.WriteLine("It's not!");
+        else Console.WriteLine("It is!");
+        Console.WriteLine("----------------------");
+    }
+    
     static void Main(string[] args)
     {
-        Person p1 = new Person();
-        p1.Age = 15;
-        Console.WriteLine(p1.Age);
+        Program p = new Program();
+        p.Palindrome("mohammed");
+        p.Palindrome("radar");
+        p.Palindrome("abccba");
     }
+}
 }
