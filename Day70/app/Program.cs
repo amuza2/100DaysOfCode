61ccd7791026369ac504220c544017df6094b4f6
Mohammed Chami <mohamuza@gmail.com>
2023-04-12 04:00:54 +0100

    update

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 namespace SoloLearn;
 class Program
 {    
+    class Dog
+    {
+        public const int ONE = 1;
+        public static int count = 1;
+        public Dog()
+        {
+            count++;
+        }
+        public static void Bark()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
     static void Main(string[] args)
     {
-        String[] pairs = { "Color1=red", "Color2=green", "Color3=blue",
-                 "Title=Code Repository" };
-        foreach (var pair in pairs) 
-        {
-            int position = pair.IndexOf("=");
-            if (position < 0)
-                continue;
-            Console.WriteLine($"Key: {pair.Substring(0, position)}, Value: '{pair.Substring(position + 1)}'");
-                            
-        }
+        Dog a = new Dog();
+        Console.WriteLine(Dog.ONE);
     }
 }
 
