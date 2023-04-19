3f9ea86b6201595466cd993a064a895fba2db2d9
Mohammed Chami <mohamuza@gmail.com>
2023-04-19 05:56:45 +0100

    sololearn generic class

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -17,17 +17,31 @@ namespace SoloLearn
         
        static void Main(string[] args)
         {
-            int a = 10;
-            string b = "Mohammed";
-            Printer.Print(b);
-
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(30, 29, 28);
+            intStack.Get();
         } 
         
-        class Printer
+        class Stack<T>
         {
-            public static void Print<T>(T a)
+            int index = 0;
+            T[] innerArray = new T[100];
+            public void Push(T item1, T item2, T item3)
             {
-                Console.WriteLine(a);
+                innerArray[index++] = item1;
+                innerArray[index++] = item2;
+                innerArray[index++] = item3;
+            }
+            public void Get()
+            {
+                foreach(T item in innerArray)
+                {
+                    Console.WriteLine(item + " ");
+                }
+            }
+            public void CheckArray(int index1, int index2, int index3)
+            {
+                Console.WriteLine($"{innerArray[index1]} - {innerArray[index2]} - {innerArray[index3]}");
             }
         }
     }        
