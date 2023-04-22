521e600d195b5910d639734261e65911684647c6
Mohammed Chami <mohamuza@gmail.com>
2023-04-22 13:56:09 +0100

    sololearn OOP

--- Day70/app/Program.cs
+++ Day70/app/Program.cs
@@ -5,40 +5,46 @@ using System.IO;
 
 namespace SoloLearn
 {
+    class Human
+        {
+            int Age {get; set;}
+            public Human(int age) {this.Age = age;}
+
+            public static bool operator> (Human a, Human b){
+                return a.Age > b.Age;
+            }
+
+            public static bool operator< (Human a, Human b){
+                return a.Age < b.Age;
+            }
+            public static bool operator== (Human a, Human b){
+                return a.Age == b.Age;
+            }
+            public static bool operator!= (Human a, Human b){
+                return a.Age != b.Age;
+            } 
+        }
     class Program
     {
         static void Output<T>(T[] value)
         {
             foreach(T i in value)
             {
-                Console.WriteLine(i);
+                Console.Write(i + " ");
             }
+            Console.WriteLine();
         }
-        public static string IsItANum(string str)
-        {
-            string res = "";
-            bool AddValue = false;
+        
 
-            foreach(char s in str)
-            {
-                if(s == '0')
-                {
-                    AddValue = true;
-                }
-                if(AddValue)
-                {
-                    bool isDigit = Char.IsDigit(s);
-                    if(isDigit) res += s;
-                }  
-            }
-            if(res.Length == 11) {return res;}
-
-            return "Not a phone number";
-        }
        static void Main(string[] args)
         {
-            string res = IsItANum("0192387415456");
-            Console.WriteLine(res);
-        }
+            Human h1 = new Human(19);
+            Human h2 = new Human(18);
+            bool isGreater = h1 == h2;
+            Console.WriteLine("h1 is greater then h2: " + isGreater);
+
+
+
+        }  
     }        
 }
