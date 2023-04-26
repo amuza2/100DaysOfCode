61ce2b7c783c2a82db1bbb5c00597a3cd8b6aeeb
Mohammed Chami <mohamuza@gmail.com>
2023-04-26 13:17:11 +0100

    new project

---
+++ Day72/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+string substring = "localhost/v1/getemp/666#123";
+
+// Create a regular expression pattern that matches any character that is not alphanumeric
+string pattern = @"^[a-zA-Z0-9]+$";
+
+// Use the Regex.IsMatch() method to check if the substring matches the pattern
+bool containsNonAlphanumeric = Regex.IsMatch(substring, pattern);
+
+if (containsNonAlphanumeric)
+{
+    Console.WriteLine("The substring contains only alphanumeric characters.");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
