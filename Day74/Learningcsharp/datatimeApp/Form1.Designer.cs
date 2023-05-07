62ec487fc68a2169f5a2f3f4e2535e1446bc7ddc
Mohammed Chami <mohamuza@gmail.com>
2023-05-07 14:24:52 +0100

    update

---
+++ Day74/Learningcsharp/datatimeApp/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace datatimeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(166, 82);
+            dateTimePicker1.Margin = new Padding(5, 5, 5, 5);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(455, 34);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(820, 587);
+            Controls.Add(dateTimePicker1);
+            Font = new Font("Monda", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+    }
+}
