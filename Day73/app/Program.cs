979e07829e25bdb52edc07f3a17c1eccb4860740
Mohammed Chami <mohamuza@gmail.com>
2023-05-24 22:23:28 +0100

    update

--- Day73/app/Program.cs
+++ Day73/app/Program.cs
@@ -5,8 +5,20 @@ namespace learning;
 
 class Program
 {
+    public static int SumOfSequence(int b, int e, int s)
+    {
+        int result = 0;
+        for(int i = b; i<=e; i += s)
+        {
+            result += i;
+        }
+        return result;
+    }
     static void Main(string[] args)
         {
-            
+            SumOfSequence(2,2,2);
+            SumOfSequence(2,6,2);
+            SumOfSequence(1,5,1);
+            SumOfSequence(1,5,3);
         }
 }
