7e5311a75699cceca83d690582b4223c4fe60539
Chami Mohammed <mohamuza@gmail.com>
2023-06-10 22:24:56 +0100

    started tictactoe game

---
+++ Day75/GamesProjects/game1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnA = new Guna.UI2.WinForms.Guna2Button();
+            this.btnB = new Guna.UI2.WinForms.Guna2Button();
+            this.btnC = new Guna.UI2.WinForms.Guna2Button();
+            this.btnD = new Guna.UI2.WinForms.Guna2Button();
+            this.btnE = new Guna.UI2.WinForms.Guna2Button();
+            this.btnF = new Guna.UI2.WinForms.Guna2Button();
+            this.btnG = new Guna.UI2.WinForms.Guna2Button();
+            this.btnH = new Guna.UI2.WinForms.Guna2Button();
+            this.btnI = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.SuspendLayout();
+            // 
+            // btnA
+            // 
+            this.btnA.Animated = true;
+            this.btnA.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnA.BorderColor = System.Drawing.Color.IndianRed;
+            this.btnA.CheckedState.Parent = this.btnA;
+            this.btnA.CustomImages.Parent = this.btnA;
+            this.btnA.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnA.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnA.ForeColor = System.Drawing.Color.White;
+            this.btnA.HoverState.Parent = this.btnA;
+            this.btnA.Location = new System.Drawing.Point(265, 132);
+            this.btnA.Name = "btnA";
+            this.btnA.PressedColor = System.Drawing.Color.DarkOrange;
+            this.btnA.ShadowDecoration.Parent = this.btnA;
+            this.btnA.Size = new System.Drawing.Size(44, 45);
+            this.btnA.TabIndex = 0;
+            this.btnA.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnB
+            // 
+            this.btnB.Animated = true;
+            this.btnB.CheckedState.Parent = this.btnB;
+            this.btnB.CustomImages.Parent = this.btnB;
+            this.btnB.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnB.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnB.ForeColor = System.Drawing.Color.White;
+            this.btnB.HoverState.Parent = this.btnB;
+            this.btnB.Location = new System.Drawing.Point(324, 132);
+            this.btnB.Name = "btnB";
+            this.btnB.ShadowDecoration.Parent = this.btnB;
+            this.btnB.Size = new System.Drawing.Size(44, 45);
+            this.btnB.TabIndex = 0;
+            this.btnB.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnC
+            // 
+            this.btnC.Animated = true;
+            this.btnC.CheckedState.Parent = this.btnC;
+            this.btnC.CustomImages.Parent = this.btnC;
+            this.btnC.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnC.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnC.ForeColor = System.Drawing.Color.White;
+            this.btnC.HoverState.Parent = this.btnC;
+            this.btnC.Location = new System.Drawing.Point(382, 132);
+            this.btnC.Name = "btnC";
+            this.btnC.ShadowDecoration.Parent = this.btnC;
+            this.btnC.Size = new System.Drawing.Size(44, 45);
+            this.btnC.TabIndex = 0;
+            this.btnC.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnD
+            // 
+            this.btnD.Animated = true;
+            this.btnD.CheckedState.Parent = this.btnD;
+            this.btnD.CustomImages.Parent = this.btnD;
+            this.btnD.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnD.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnD.ForeColor = System.Drawing.Color.White;
+            this.btnD.HoverState.Parent = this.btnD;
+            this.btnD.Location = new System.Drawing.Point(265, 188);
+            this.btnD.Name = "btnD";
+            this.btnD.ShadowDecoration.Parent = this.btnD;
+            this.btnD.Size = new System.Drawing.Size(44, 45);
+            this.btnD.TabIndex = 0;
+            this.btnD.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnE
+            // 
+            this.btnE.Animated = true;
+            this.btnE.CheckedState.Parent = this.btnE;
+            this.btnE.CustomImages.Parent = this.btnE;
+            this.btnE.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnE.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnE.ForeColor = System.Drawing.Color.White;
+            this.btnE.HoverState.Parent = this.btnE;
+            this.btnE.Location = new System.Drawing.Point(324, 188);
+            this.btnE.Name = "btnE";
+            this.btnE.ShadowDecoration.Parent = this.btnE;
+            this.btnE.Size = new System.Drawing.Size(44, 45);
+            this.btnE.TabIndex = 0;
+            this.btnE.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnF
+            // 
+            this.btnF.Animated = true;
+            this.btnF.CheckedState.Parent = this.btnF;
+            this.btnF.CustomImages.Parent = this.btnF;
+            this.btnF.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnF.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnF.ForeColor = System.Drawing.Color.White;
+            this.btnF.HoverState.Parent = this.btnF;
+            this.btnF.Location = new System.Drawing.Point(382, 188);
+            this.btnF.Name = "btnF";
+            this.btnF.ShadowDecoration.Parent = this.btnF;
+            this.btnF.Size = new System.Drawing.Size(44, 45);
+            this.btnF.TabIndex = 0;
+            this.btnF.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnG
+            // 
+            this.btnG.Animated = true;
+            this.btnG.CheckedState.Parent = this.btnG;
+            this.btnG.CustomImages.Parent = this.btnG;
+            this.btnG.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnG.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnG.ForeColor = System.Drawing.Color.White;
+            this.btnG.HoverState.Parent = this.btnG;
+            this.btnG.Location = new System.Drawing.Point(265, 244);
+            this.btnG.Name = "btnG";
+            this.btnG.ShadowDecoration.Parent = this.btnG;
+            this.btnG.Size = new System.Drawing.Size(44, 45);
+            this.btnG.TabIndex = 0;
+            this.btnG.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnH
+            // 
+            this.btnH.Animated = true;
+            this.btnH.CheckedState.Parent = this.btnH;
+            this.btnH.CustomImages.Parent = this.btnH;
+            this.btnH.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnH.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnH.ForeColor = System.Drawing.Color.White;
+            this.btnH.HoverState.Parent = this.btnH;
+            this.btnH.Location = new System.Drawing.Point(324, 244);
+            this.btnH.Name = "btnH";
+            this.btnH.ShadowDecoration.Parent = this.btnH;
+            this.btnH.Size = new System.Drawing.Size(44, 45);
+            this.btnH.TabIndex = 0;
+            this.btnH.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // btnI
+            // 
+            this.btnI.Animated = true;
+            this.btnI.CheckedState.Parent = this.btnI;
+            this.btnI.CustomImages.Parent = this.btnI;
+            this.btnI.FillColor = System.Drawing.Color.LightSalmon;
+            this.btnI.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnI.ForeColor = System.Drawing.Color.White;
+            this.btnI.HoverState.Parent = this.btnI;
+            this.btnI.Location = new System.Drawing.Point(382, 244);
+            this.btnI.Name = "btnI";
+            this.btnI.ShadowDecoration.Parent = this.btnI;
+            this.btnI.Size = new System.Drawing.Size(44, 45);
+            this.btnI.TabIndex = 0;
+            this.btnI.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnI);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnH);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnA;
+        private Guna.UI2.WinForms.Guna2Button btnB;
+        private Guna.UI2.WinForms.Guna2Button btnC;
+        private Guna.UI2.WinForms.Guna2Button btnD;
+        private Guna.UI2.WinForms.Guna2Button btnE;
+        private Guna.UI2.WinForms.Guna2Button btnF;
+        private Guna.UI2.WinForms.Guna2Button btnG;
+        private Guna.UI2.WinForms.Guna2Button btnH;
+        private Guna.UI2.WinForms.Guna2Button btnI;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
+    }
+}
+
---
+++ Day75/GamesProjects/game1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game1
+{
+    public partial class Form1 : Form
+    {
+        
+        char who = 'o';
+        int turnCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+                        
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Guna2Button bt = sender as Guna2Button;
+            
+            if(bt != null )
+            {
+                if(who == 'o') bt.Text = "O"; else bt.Text = "X";
+                if (who == 'o') who = 'x'; else who = 'o';
+                bt.Enabled = false;
+                turnCounter++;
+                bt.ForeColor = Color.Red;
+                bt.BackColor = Color.Red;
+                CheckTheWinner(bt);
+            }
+            
+        }
+
+        private void ChangeBackColor(Guna2Button button1, Guna2Button button2, Guna2Button button3)
+        {
+            button1.BackColor = Color.Green;
+            button2.BackColor = Color.Green;
+            button3.BackColor = Color.Green;
+        }
+
+        // Horizantal
+        private bool HLine1()
+        {
+            if (btnA.Text == btnB.Text && btnB.Text == btnC.Text)
+            {
+                ChangeBackColor(btnA, btnB, btnC);
+                return true;
+            }
+            return false;
+        }
+        private bool HLine2()
+        {
+            if (btnD.Text == btnE.Text && btnE.Text == btnF.Text)
+            {
+                ChangeBackColor(btnD, btnE, btnF);
+                return true;
+            }
+            return false;
+        }
+        private bool HLine3()
+        {
+            if (btnG.Text == btnH.Text && btnH.Text == btnI.Text)
+            {
+                ChangeBackColor(btnG, btnH, btnI);
+                return true;
+            }
+            return false;
+        }
+        // Vertical
+        private bool VLine1()
+        {
+            if (btnA.Text == btnD.Text && btnD.Text == btnG.Text)
+            {
+                ChangeBackColor(btnA, btnD, btnG);
+                return true;
+            }
+            return false;
+        }
+        private bool VLine2()
+        {
+            if (btnB.Text == btnE.Text && btnE.Text == btnH.Text)
+            {
+                ChangeBackColor(btnB, btnE, btnH);
+                return true;
+            }
+            return false;
+        }
+        private bool VLine3()
+        {
+            if (btnC.Text == btnF.Text && btnF.Text == btnI.Text)
+            {
+                ChangeBackColor(btnC, btnF, btnI);
+                return true;
+            }
+            return false;
+        }
+        // Diagonal
+        private bool DLine1()
+        {
+            if (btnA.Text == btnE.Text && btnE.Text == btnI.Text)
+            {
+                ChangeBackColor(btnA, btnE, btnI);
+                return true;
+            }
+            return false;
+        }
+        private bool DLine2()
+        {
+            if (btnG.Text == btnE.Text && btnE.Text == btnC.Text)
+            {
+                ChangeBackColor(btnG, btnE, btnC);
+                return true;
+            }
+            return false;
+        }
+
+        private void CheckTheWinner(Guna2Button bt)
+        {
+            bool isWinner = false;
+
+
+            switch (bt.Name)
+            {
+                case "btnA":
+                    if(HLine1() || VLine1() || DLine1()) isWinner = true;
+                    break;
+                case "btn.B":
+                    if(HLine1() || VLine2()) isWinner = true;
+                    break;
+                case "btnC":
+                    if(HLine1() || VLine3() || DLine2()) isWinner = true;
+                    break;
+                case "btnD":
+
+                    break;
+            }
+        }
+    }
+}
