ec9fd275e3ce69071ae375d56e0da78f77365574
Chami Mohammed <mohamuza@gmail.com>
2023-06-25 00:56:36 +0100

    updated

--- Day74/Learningcsharp/ageProgram/Program.cs
+++ Day74/Learningcsharp/ageProgram/Program.cs
@@ -6,9 +6,23 @@ using System.Collections.Generic;
 
 class Program
 {
+    class Shape
+    {
+
+    }
+    class Square : Shape
+    {
+
+    }
     static void Main(string[] args)
     {
-        
+        Shape shape = new Shape();
+        Square square = new Square();
+        Shape s = new Square();
+
+        Console.WriteLine(shape.GetType());
+        Console.WriteLine(square.GetType());
+        Console.WriteLine(s.GetType());
     }
         
 }
