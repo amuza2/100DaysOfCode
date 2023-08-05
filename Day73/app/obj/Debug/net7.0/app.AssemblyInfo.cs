20b332713fe96b3e1bad6e50f75fd677448fdae0
Chami Mohammed <mohamuza@gmail.com>
2023-08-05 22:03:25 +0100

    update

--- Day73/app/obj/Debug/net7.0/app.AssemblyInfo.cs
+++ Day73/app/obj/Debug/net7.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c6b48cde203d00d5346e580aa5a987905348ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
