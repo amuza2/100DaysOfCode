94c0414c276577cb9659cb784d84c5e0d420d911
Chami Mohammed <mohamuza@gmail.com>
2023-10-17 22:42:10 +0100

    feat: added new data proj that deals with database

---
+++ Day79/Database app/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_app
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand titlesQuery;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Books.accdb;Persist Security Info=False;";
+            conn = new OleDbConnection(connString);
+            conn.Open();
+            titlesQuery = new OleDbCommand("SELECT * FROM Titles", conn);
+            conn.Close();
+            conn.Dispose();
+
+
+        }
+    }
+}
