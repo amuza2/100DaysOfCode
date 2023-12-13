2f862f4cb998c5eb184d6e7e3271f0534b6de456
MohammedChami <mohamuza@gmail.com>
2023-12-13 17:40:09 +0100

    update codewars file

--- Day81/learning/Challenges/Program.cs
+++ Day81/learning/Challenges/Program.cs
@@ -11,26 +11,38 @@ namespace CodeWars
 {
     internal class Program
     {
-        public static string CuckooClock(string inputTime, int chimes)
+        public static string ReverseWords(string str)
         {
-            string[] splitTime = inputTime.Split(':');
-            int hour = int.Parse(splitTime[0]);
-            int minute = int.Parse(splitTime[1]);
-            int leftTimeInHour = Math.Abs(59 - minute);
-            int champsCounter = 0;
-            for (int i = 0; i < 12; i++)
+            string result = "";
+            string[] arr =  str.Split(' ');
+            foreach (var item in arr)
             {
-                champsCounter += leftTimeInHour % 15;
-                if (minute == 0) champsCounter += hour;
-                
-                hour++;
-
+                result += item.Reverse().ToString() + " ";
+                Console.WriteLine(result);
             }
-            return inputTime;
+            return result.Trim();
         }
+
+        //public static string CuckooClock(string inputTime, int chimes)
+        //{
+        //    string[] splitTime = inputTime.Split(':');
+        //    int hour = int.Parse(splitTime[0]);
+        //    int minute = int.Parse(splitTime[1]);
+        //    int leftTimeInHour = Math.Abs(59 - minute);
+        //    int champsCounter = 0;
+        //    for (int i = 0; i < 12; i++)
+        //    {
+        //        champsCounter += leftTimeInHour % 15;
+        //        if (minute == 0) champsCounter += hour;
+
+        //        hour++;
+
+        //    }
+        //    return inputTime;
+        //}
         static void Main(string[] args)
         {
-
+            Console.WriteLine(ReverseWords("This is an example!"));
         }
     }
 }
