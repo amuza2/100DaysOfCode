39070b54eb7a97738b6cd6339e37ce143b26a461
MohammedChami <mohamuza@gmail.com>
2024-01-07 07:56:56 +0100

    feat: centralisation started

--- Day81/learning/Challenges/Program.cs
+++ Day81/learning/Challenges/Program.cs
@@ -6,8 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CodeWars
 {
@@ -41,43 +40,15 @@ namespace CodeWars
             }
             Console.WriteLine();
         }
-        public static string[] greek_alphabet = { "alpha", "beta", "gamma", "delta", "epsilon",
-            "zeta", "eta", "theta", "iota", "kappa", "lambda", 
-            "mu", "nu", "xi", "omicron", "pi", "rho", "sigma", "tau", 
-            "upsilon", "phi", "chi", "psi", "omega" };
+        public static bool HasSubpattern(string str)
+        {
 
-        public static int greekComparator(string lhs, string rhs)
-        {
-            if (lhs == rhs) return 0;
-            else
-            {
-                int result = 0;
-                if (lhs[0] < rhs[0]) result = -1;
-                else result = 1;
-                return result;
-            }
-        }
-        public static void pairs(string[] pairs)
-        {
-            var random = new Random();
-            for (int i = 0; i < greek_alphabet.Length; i++)
-            {
-                var pair = new { x = greek_alphabet[random.Next(greek_alphabet.Length)], y = greek_alphabet[random.Next(greek_alphabet.Length)]};
-                int resultTest = pair.x == pair.y ? 0 : Array.IndexOf(greek_alphabet, pair.x) < Array.IndexOf(greek_alphabet, pair.y) ? -1 : 1;
-                int userResult = Solution.GreekComparator(pair.x, pair.y);
-                if (userResult == 0)
-                {
-                    Assert.AreEqual(userResult, 0);
-                }
-                else if (userResult == 1) Assert.Greater(userResult, 0);    
-            }
+            return false;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(greekComparator("alpha", "beta"));
-            Console.WriteLine(greekComparator("chi", "chi"));
-            Console.WriteLine(greekComparator("upsilon", "rho"));
+            
 
         }
     }
