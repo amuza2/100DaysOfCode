49d9747f79a3cabd30552a44892b050afcdade75
MohammedChami <mohamuza@gmail.com>
2024-01-18 22:00:46 +0100

    Update Comic Book Detail view

--- Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
+++ Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
@@ -10,6 +10,17 @@ namespace comicBook.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.Serietitle = "The amazing Spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
         }
         
