6e0d4c0557912ffcd6595470af47f476d52a74cc
MohammedChami <mohamuza@gmail.com>
2024-01-19 21:28:42 +0100

    Added comic book and artist model

---
+++ Day84/comicBook/comicBook/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBook.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Day84/comicBook/comicBook/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBook.Models
+{
+    public class ComicBook
+    {
+        public int Id {  get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNupber { get; set; }
+        public int DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+    }
+}
