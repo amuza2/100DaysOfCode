8b35af3fd76a564b422559c184e694724ea1e66f
MohammedChami <mohamuza@gmail.com>
2024-01-20 01:41:53 +0100

    Updated comic book detail view

--- Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
+++ Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using comicBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,18 +11,21 @@ namespace comicBook.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.Serietitle = "The amazing Spider-man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The amazing Spider-man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Script", Role= "Dan Slott" },
+                    new Artist() {Name = "Pencils", Role= "Humberto Ramos" },
+                    new Artist() {Name = "Inks", Role= "Victor Olazaba" },
+                    new Artist() {Name = "Colors", Role= "Edgar Delgado" },
+                    new Artist() {Name = "Letters", Role= "Chris Eliopoulos" },
+                },
             };
-            return View();
+            return View(comicBook);
         }
         
     }
--- Day84/comicBook/comicBook/Models/ComicBook.cs
+++ Day84/comicBook/comicBook/Models/ComicBook.cs
@@ -10,7 +10,7 @@ namespace comicBook.Models
         public int Id {  get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public int DescriptionHtml { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
