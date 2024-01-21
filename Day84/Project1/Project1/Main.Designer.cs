f702fd67e278938baa4c2db7b9992a37bc92c280
MohammedChami <mohamuza@gmail.com>
2024-01-21 18:34:54 +0100

    create form 1

---
+++ Day84/Project1/Project1/Main.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Project1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbEmploye = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.grbDate = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbInscription = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.grbEmploye.SuspendLayout();
+            this.grbDate.SuspendLayout();
+            this.grbInscription.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbEmploye
+            // 
+            this.grbEmploye.Controls.Add(this.button5);
+            this.grbEmploye.Controls.Add(this.button4);
+            this.grbEmploye.Controls.Add(this.button3);
+            this.grbEmploye.Controls.Add(this.button2);
+            this.grbEmploye.Controls.Add(this.button1);
+            this.grbEmploye.Location = new System.Drawing.Point(39, 22);
+            this.grbEmploye.Name = "grbEmploye";
+            this.grbEmploye.Size = new System.Drawing.Size(718, 244);
+            this.grbEmploye.TabIndex = 0;
+            this.grbEmploye.TabStop = false;
+            this.grbEmploye.Text = "Employe";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Location = new System.Drawing.Point(38, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ajouter Employe";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.Location = new System.Drawing.Point(38, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 36);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ajouter Projet";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Location = new System.Drawing.Point(38, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(265, 36);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Consulter Employe";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button4.Location = new System.Drawing.Point(415, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(265, 36);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Modifier Employe";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button5.Location = new System.Drawing.Point(415, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(265, 36);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Rechercher Employe";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // grbDate
+            // 
+            this.grbDate.Controls.Add(this.label1);
+            this.grbDate.Location = new System.Drawing.Point(39, 273);
+            this.grbDate.Name = "grbDate";
+            this.grbDate.Size = new System.Drawing.Size(327, 84);
+            this.grbDate.TabIndex = 1;
+            this.grbDate.TabStop = false;
+            this.grbDate.Text = "Date du jour";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // grbInscription
+            // 
+            this.grbInscription.Controls.Add(this.button7);
+            this.grbInscription.Controls.Add(this.button6);
+            this.grbInscription.Location = new System.Drawing.Point(387, 273);
+            this.grbInscription.Name = "grbInscription";
+            this.grbInscription.Size = new System.Drawing.Size(370, 145);
+            this.grbInscription.TabIndex = 2;
+            this.grbInscription.TabStop = false;
+            this.grbInscription.Text = "Inscription";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button6.Location = new System.Drawing.Point(67, 24);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(265, 36);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Consultation";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button7.Location = new System.Drawing.Point(67, 79);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(265, 36);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Creation";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbInscription);
+            this.Controls.Add(this.grbDate);
+            this.Controls.Add(this.grbEmploye);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.grbEmploye.ResumeLayout(false);
+            this.grbDate.ResumeLayout(false);
+            this.grbDate.PerformLayout();
+            this.grbInscription.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbEmploye;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox grbDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbInscription;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
