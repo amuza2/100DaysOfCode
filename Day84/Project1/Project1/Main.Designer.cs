03d8dae2a8283ee3d2072e39a56db7d18456800d
MohammedChami <mohamuza@gmail.com>
2024-01-21 19:23:42 +0100

    names all controls of Main

--- Day84/Project1/Project1/Main.Designer.cs
+++ Day84/Project1/Project1/Main.Designer.cs
@@ -29,140 +29,173 @@
         private void InitializeComponent()
         {
             this.grbEmploye = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnEmployeAjouter = new System.Windows.Forms.Button();
+            this.btnProjetAjouter = new System.Windows.Forms.Button();
+            this.btnEmployeConsulter = new System.Windows.Forms.Button();
+            this.btnEmployeModifier = new System.Windows.Forms.Button();
+            this.btnEmployeRechercher = new System.Windows.Forms.Button();
             this.grbDate = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
             this.grbInscription = new System.Windows.Forms.GroupBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.btnInscriptionConsultation = new System.Windows.Forms.Button();
+            this.btnInscriptionCreation = new System.Windows.Forms.Button();
+            this.grbProjet = new System.Windows.Forms.GroupBox();
+            this.btnProjetConsulter = new System.Windows.Forms.Button();
             this.grbEmploye.SuspendLayout();
             this.grbDate.SuspendLayout();
             this.grbInscription.SuspendLayout();
+            this.grbProjet.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbEmploye
             // 
-            this.grbEmploye.Controls.Add(this.button5);
-            this.grbEmploye.Controls.Add(this.button4);
-            this.grbEmploye.Controls.Add(this.button3);
-            this.grbEmploye.Controls.Add(this.button2);
-            this.grbEmploye.Controls.Add(this.button1);
+            this.grbEmploye.Controls.Add(this.btnEmployeRechercher);
+            this.grbEmploye.Controls.Add(this.btnEmployeModifier);
+            this.grbEmploye.Controls.Add(this.btnEmployeConsulter);
+            this.grbEmploye.Controls.Add(this.btnEmployeAjouter);
             this.grbEmploye.Location = new System.Drawing.Point(39, 22);
             this.grbEmploye.Name = "grbEmploye";
-            this.grbEmploye.Size = new System.Drawing.Size(718, 244);
+            this.grbEmploye.Size = new System.Drawing.Size(336, 244);
             this.grbEmploye.TabIndex = 0;
             this.grbEmploye.TabStop = false;
             this.grbEmploye.Text = "Employe";
             // 
-            // button1
+            // btnEmployeAjouter
             // 
-            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button1.Location = new System.Drawing.Point(38, 37);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(265, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ajouter Employe";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnEmployeAjouter.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnEmployeAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployeAjouter.Location = new System.Drawing.Point(39, 19);
+            this.btnEmployeAjouter.Name = "btnEmployeAjouter";
+            this.btnEmployeAjouter.Size = new System.Drawing.Size(265, 36);
+            this.btnEmployeAjouter.TabIndex = 0;
+            this.btnEmployeAjouter.Text = "Ajouter Employe";
+            this.btnEmployeAjouter.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnProjetAjouter
             // 
-            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button2.Location = new System.Drawing.Point(38, 94);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(265, 36);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Ajouter Projet";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnProjetAjouter.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnProjetAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProjetAjouter.Location = new System.Drawing.Point(67, 19);
+            this.btnProjetAjouter.Name = "btnProjetAjouter";
+            this.btnProjetAjouter.Size = new System.Drawing.Size(265, 36);
+            this.btnProjetAjouter.TabIndex = 0;
+            this.btnProjetAjouter.Text = "Ajouter Projet";
+            this.btnProjetAjouter.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btnEmployeConsulter
             // 
-            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button3.Location = new System.Drawing.Point(38, 154);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(265, 36);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Consulter Employe";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnEmployeConsulter.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnEmployeConsulter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployeConsulter.Location = new System.Drawing.Point(39, 187);
+            this.btnEmployeConsulter.Name = "btnEmployeConsulter";
+            this.btnEmployeConsulter.Size = new System.Drawing.Size(265, 36);
+            this.btnEmployeConsulter.TabIndex = 0;
+            this.btnEmployeConsulter.Text = "Consulter Employe";
+            this.btnEmployeConsulter.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // btnEmployeModifier
             // 
-            this.button4.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button4.Location = new System.Drawing.Point(415, 37);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(265, 36);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Modifier Employe";
-            this.button4.UseVisualStyleBackColor = false;
+            this.btnEmployeModifier.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnEmployeModifier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployeModifier.Location = new System.Drawing.Point(39, 75);
+            this.btnEmployeModifier.Name = "btnEmployeModifier";
+            this.btnEmployeModifier.Size = new System.Drawing.Size(265, 36);
+            this.btnEmployeModifier.TabIndex = 0;
+            this.btnEmployeModifier.Text = "Modifier Employe";
+            this.btnEmployeModifier.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // btnEmployeRechercher
             // 
-            this.button5.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button5.Location = new System.Drawing.Point(415, 94);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(265, 36);
-            this.button5.TabIndex = 0;
-            this.button5.Text = "Rechercher Employe";
-            this.button5.UseVisualStyleBackColor = false;
+            this.btnEmployeRechercher.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnEmployeRechercher.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployeRechercher.Location = new System.Drawing.Point(39, 131);
+            this.btnEmployeRechercher.Name = "btnEmployeRechercher";
+            this.btnEmployeRechercher.Size = new System.Drawing.Size(265, 36);
+            this.btnEmployeRechercher.TabIndex = 0;
+            this.btnEmployeRechercher.Text = "Rechercher Employe";
+            this.btnEmployeRechercher.UseVisualStyleBackColor = false;
             // 
             // grbDate
             // 
-            this.grbDate.Controls.Add(this.label1);
+            this.grbDate.Controls.Add(this.lblDate);
             this.grbDate.Location = new System.Drawing.Point(39, 273);
             this.grbDate.Name = "grbDate";
-            this.grbDate.Size = new System.Drawing.Size(327, 84);
+            this.grbDate.Size = new System.Drawing.Size(336, 67);
             this.grbDate.TabIndex = 1;
             this.grbDate.TabStop = false;
             this.grbDate.Text = "Date du jour";
             // 
-            // label1
+            // lblDate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(35, 30);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(51, 20);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "label1";
             // 
             // grbInscription
             // 
-            this.grbInscription.Controls.Add(this.button7);
-            this.grbInscription.Controls.Add(this.button6);
-            this.grbInscription.Location = new System.Drawing.Point(387, 273);
+            this.grbInscription.Controls.Add(this.btnInscriptionCreation);
+            this.grbInscription.Controls.Add(this.btnInscriptionConsultation);
+            this.grbInscription.Location = new System.Drawing.Point(387, 158);
             this.grbInscription.Name = "grbInscription";
             this.grbInscription.Size = new System.Drawing.Size(370, 145);
             this.grbInscription.TabIndex = 2;
             this.grbInscription.TabStop = false;
             this.grbInscription.Text = "Inscription";
             // 
-            // button6
+            // btnInscriptionConsultation
             // 
-            this.button6.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button6.Location = new System.Drawing.Point(67, 24);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(265, 36);
-            this.button6.TabIndex = 0;
-            this.button6.Text = "Consultation";
-            this.button6.UseVisualStyleBackColor = false;
+            this.btnInscriptionConsultation.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnInscriptionConsultation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscriptionConsultation.Location = new System.Drawing.Point(67, 30);
+            this.btnInscriptionConsultation.Name = "btnInscriptionConsultation";
+            this.btnInscriptionConsultation.Size = new System.Drawing.Size(265, 36);
+            this.btnInscriptionConsultation.TabIndex = 0;
+            this.btnInscriptionConsultation.Text = "Consultation";
+            this.btnInscriptionConsultation.UseVisualStyleBackColor = false;
             // 
-            // button7
+            // btnInscriptionCreation
             // 
-            this.button7.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button7.Location = new System.Drawing.Point(67, 79);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(265, 36);
-            this.button7.TabIndex = 0;
-            this.button7.Text = "Creation";
-            this.button7.UseVisualStyleBackColor = false;
+            this.btnInscriptionCreation.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnInscriptionCreation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscriptionCreation.Location = new System.Drawing.Point(67, 88);
+            this.btnInscriptionCreation.Name = "btnInscriptionCreation";
+            this.btnInscriptionCreation.Size = new System.Drawing.Size(265, 36);
+            this.btnInscriptionCreation.TabIndex = 0;
+            this.btnInscriptionCreation.Text = "Creation";
+            this.btnInscriptionCreation.UseVisualStyleBackColor = false;
+            // 
+            // grbProjet
+            // 
+            this.grbProjet.Controls.Add(this.btnProjetConsulter);
+            this.grbProjet.Controls.Add(this.btnProjetAjouter);
+            this.grbProjet.Location = new System.Drawing.Point(387, 22);
+            this.grbProjet.Name = "grbProjet";
+            this.grbProjet.Size = new System.Drawing.Size(370, 130);
+            this.grbProjet.TabIndex = 3;
+            this.grbProjet.TabStop = false;
+            this.grbProjet.Text = "Projet";
+            // 
+            // btnProjetConsulter
+            // 
+            this.btnProjetConsulter.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnProjetConsulter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProjetConsulter.Location = new System.Drawing.Point(67, 75);
+            this.btnProjetConsulter.Name = "btnProjetConsulter";
+            this.btnProjetConsulter.Size = new System.Drawing.Size(265, 36);
+            this.btnProjetConsulter.TabIndex = 0;
+            this.btnProjetConsulter.Text = "Consulter Projet";
+            this.btnProjetConsulter.UseVisualStyleBackColor = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 382);
+            this.Controls.Add(this.grbProjet);
             this.Controls.Add(this.grbInscription);
             this.Controls.Add(this.grbDate);
             this.Controls.Add(this.grbEmploye);
@@ -172,6 +205,7 @@
             this.grbDate.ResumeLayout(false);
             this.grbDate.PerformLayout();
             this.grbInscription.ResumeLayout(false);
+            this.grbProjet.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -179,16 +213,18 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grbEmploye;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEmployeRechercher;
+        private System.Windows.Forms.Button btnEmployeModifier;
+        private System.Windows.Forms.Button btnEmployeConsulter;
+        private System.Windows.Forms.Button btnProjetAjouter;
+        private System.Windows.Forms.Button btnEmployeAjouter;
         private System.Windows.Forms.GroupBox grbDate;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
         private System.Windows.Forms.GroupBox grbInscription;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnInscriptionCreation;
+        private System.Windows.Forms.Button btnInscriptionConsultation;
+        private System.Windows.Forms.GroupBox grbProjet;
+        private System.Windows.Forms.Button btnProjetConsulter;
     }
 }
 
