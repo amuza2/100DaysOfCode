8c1b52b7f3ce4d24b87cb4d19c24d78a59e31104
MohammedChami <mohamuza@gmail.com>
2024-04-25 22:42:43 +0100

    update: entity framework project

--- Day82/EntiryFrameworkCleanStart/DatabaseFirstDemoEF/Program.cs
+++ Day82/EntiryFrameworkCleanStart/DatabaseFirstDemoEF/Program.cs
@@ -10,7 +10,16 @@ namespace DatabaseFirstDemoEF
     {
         static void Main(string[] args)
         {
-
+            var context = new cenimaDatabaseEntities();
+            var film = new film()
+            {
+                id_realisator = 1,
+                title_film = "EF Test Title",
+                length_film = "2h:45m",
+                released_date = DateTime.Now,
+            };
+            context.films.Add(film);
+            context.SaveChanges();
         }
     }
 }
