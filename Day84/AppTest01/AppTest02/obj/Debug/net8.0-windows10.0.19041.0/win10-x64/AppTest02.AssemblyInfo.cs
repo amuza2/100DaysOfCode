2d916981faaa4469b450cfe4d008e043a514b4cd
MohammedChami <mohamuza@gmail.com>
2024-05-10 12:08:23 +0100

    updated AppTest01

--- Day84/AppTest01/AppTest02/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTest02.AssemblyInfo.cs
+++ Day84/AppTest01/AppTest02/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTest02.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTest02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8c1b52b7f3ce4d24b87cb4d19c24d78a59e31104")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a6c03b16f23693b3ac90e0aa6b61815a2edccfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTest02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTest02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
