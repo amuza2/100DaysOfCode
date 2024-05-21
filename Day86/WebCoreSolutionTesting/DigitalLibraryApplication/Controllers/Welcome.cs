8cb4358b836c4296e8bfb2455d4148a9369623f6
MohammedChami <mohamuza@gmail.com>
2024-05-21 22:36:49 +0100

    added a new asp.net core mvc project

---
+++ Day86/WebCoreSolutionTesting/DigitalLibraryApplication/Controllers/Welcome.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalLibraryApplication.Controllers
+{
+    public class Welcome : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to digital library";
+            return View();
+        }
+        public string Description()
+        {
+            return "This is the application for managing digital books";
+        }
+    }
+}
