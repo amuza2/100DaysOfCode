5d7b4bcd3b0f24f8d3db13c2979bfc318ef5295c
MohammedChami <mohamuza@gmail.com>
2024-05-21 22:36:02 +0100

    Added comic book repository

--- Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
+++ Day84/comicBook/comicBook/Controllers/ComicBooksController.cs
@@ -11,20 +11,7 @@ namespace comicBook.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The amazing Spider-man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name = "Script", Role= "Dan Slott" },
-                    new Artist() {Name = "Pencils", Role= "Humberto Ramos" },
-                    new Artist() {Name = "Inks", Role= "Victor Olazaba" },
-                    new Artist() {Name = "Colors", Role= "Edgar Delgado" },
-                    new Artist() {Name = "Letters", Role= "Chris Eliopoulos" },
-                },
-            };
+            
             return View(comicBook);
         }
 
---
+++ Day84/comicBook/comicBook/Data/ComicBookRepository.cs
@@ -0,0 +1,75 @@
+﻿using comicBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBook.Data
+{
+	public class ComicBookRepository
+	{
+		private static ComicBook[] _comicBooks = new ComicBook[]
+		{
+			new ComicBook()
+			{
+				Id = 1,
+				SeriesTitle = "The amazing Spider-man",
+				IssueNumber = 700,
+				DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
+				Artists = new Artist[]
+				{
+					new Artist() {Name = "Script", Role= "Dan Slott" },
+					new Artist() {Name = "Pencils", Role= "Humberto Ramos" },
+					new Artist() {Name = "Inks", Role= "Victor Olazaba" },
+					new Artist() {Name = "Colors", Role= "Edgar Delgado" },
+					new Artist() {Name = "Letters", Role= "Chris Eliopoulos" },
+				},
+			},
+			new ComicBook()
+			{
+				Id = 2,
+				SeriesTitle = "The Amazing Spider-Man",
+				IssueNumber = 657,
+				DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+				Artists = new Artist[]
+				{
+					new Artist() { Name = "Dan Slott", Role = "Script" },
+					new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+					new Artist() { Name = "Marcos Martin", Role = "Inks" },
+					new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+					new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+				},
+				Favorite = false
+			},
+			new ComicBook()
+			{
+				Id = 3,
+				SeriesTitle = "Bone",
+				IssueNumber = 50,
+				DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+				Artists = new Artist[]
+				{
+					new Artist() { Name = "Jeff Smith", Role = "Script" },
+					new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+					new Artist() { Name = "Jeff Smith", Role = "Inks" },
+					new Artist() { Name = "Jeff Smith", Role = "Letters" }
+				},
+				Favorite = false
+			}
+
+	};
+		public ComicBook GetComicBook(int id)
+		{
+			ComicBook selectedComicBook = null;
+            foreach (var book in _comicBooks)
+            {
+                if(book.Id == id)
+				{
+					selectedComicBook = book;
+					break;
+				}
+            }
+			return selectedComicBook;
+        }
+	}
+}
