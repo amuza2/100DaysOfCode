79315f79a1d70d30cbcc6f312d984556d5e7abff
MohammedChami <mohamuza@gmail.com>
2025-04-06 10:43:19 +0100

    updated WPF simple project

---
+++ Day93/WpfApp1/SimpleWpfApp/Converters/ButtonTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SimpleWpfApp.Converters;
+
+public class ButtonTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isEditing = (bool)value;
+        return isEditing ? "Update User" : "Add User";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
