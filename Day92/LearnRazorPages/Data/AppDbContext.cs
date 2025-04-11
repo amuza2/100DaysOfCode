2305117b3fb9a818a370a63ae350a07586a11582
MohammedChami <mohamuza@gmail.com>
2025-04-11 19:13:51 +0100

    added razor page project

---
+++ Day92/LearnRazorPages/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearnRazorPages;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+}
---
+++ Day92/LearnRazorPages/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnRazorPages;
+
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Author { get; set; }
+}
